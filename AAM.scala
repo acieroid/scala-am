019dd4cfef55aafa940de3546c425324fb86862b
Quentin Stievenart <quentin.stievenart@gmail.com>
2014-12-16 12:57:14 +0100

    Support let

--- AAM.scala
+++ AAM.scala
@@ -55,9 +55,15 @@ object Env {
 }
 
 sealed abstract class Kont
-case class KontLet(v: String, body: ANFExp, env: Env, addr: KontAddress) extends Kont
-case class KontLetrec(v: String, body: ANFExp, env: Env, addr: KontAddress) extends Kont
-case class KontHalt extends Kont
+case class KontLet(v: String, body: ANFExp, env: Env, next: KontAddress) extends Kont {
+  override def toString(): String = s"KontLet(${v.toString})"
+}
+case class KontLetrec(v: String, a: Address, body: ANFExp, env: Env, next: KontAddress) extends Kont {
+  override def toString(): String = s"KontLetrec(${v.toString})"
+}
+case class KontHalt extends Kont {
+  override def toString(): String = s"KontHalt()"
+}
 
 sealed abstract class AbstractValue {
   def isTrue: Boolean
@@ -84,8 +90,12 @@ case class AbstractKont(κ: Kont) extends AbstractValue {
 }
 
 sealed abstract class Control
-case class ControlEval(exp: ANFExp, env: Env) extends Control
-case class ControlKont(v: AbstractValue) extends Control
+case class ControlEval(exp: ANFExp, env: Env) extends Control {
+  override def toString(): String = s"ev(${exp.toString})"
+}
+case class ControlKont(v: AbstractValue) extends Control {
+  override def toString(): String = s"ko(${v.toString})"
+}
 
 /** TODO: use a more generic lattice? */
 case class Store(content: Map[Address, Set[AbstractValue]]) {
@@ -97,34 +107,46 @@ case class Store(content: Map[Address, Set[AbstractValue]]) {
   def ++(l: List[(Address, AbstractValue)]): Store = l.foldLeft(this)((σ, v) => σ ⊔ v)
 }
 object Store {
-  val initial = new Store() ++ Primitives.forStore
+  val haltAddress = KontAddress(ANFQuoted(SExpIdentifier("halt")))
+  val initial = new Store() ++ List(haltAddress -> AbstractKont(KontHalt())) ++ Primitives.forStore
 }
 
-case class State(control: Control, σ: Store, κ: Kont) {
-  def this(exp: ANFExp) = this(ControlEval(exp, Env.initial), Store.initial, KontHalt())
+case class State(control: Control, σ: Store, a: KontAddress) {
+  /** Inject an expression into a state */
+  def this(exp: ANFExp) = this(ControlEval(exp, Env.initial), Store.initial, Store.haltAddress)
+
+  override def toString(): String = control.toString
+
   def step: Set[State] = control match {
-    case ControlEval(e, ρ) => stepEval(e, ρ, σ, κ)
-    case ControlKont(v) => stepKont(v, σ, κ)
+    case ControlEval(e, ρ) => stepEval(e, ρ, σ, a)
+    case ControlKont(v) => σ(a).foldLeft(Set[State]())((s, kont) => kont match {
+      case AbstractKont(κ) => s ++ stepKont(v, σ, κ)
+      case _ => s
+    })
   }
 
+  /** Check whether this state is in a final configuration */
   def halted: Boolean = control match {
     case ControlEval(_, _) => false
-    case ControlKont(v) => κ match {
-      case KontHalt() => true
-      case _ => false
-    }
+    case ControlKont(v) => this.a == Store.haltAddress
   }
 
   def atomicEval(e: ANFAtomicExp, ρ: Env, σ: Store): Set[AbstractValue] = e match {
     case λ: ANFLambda => Set(AbstractClosure(λ, ρ))
-    case ANFIdentifier(name) => ρ(name) match {
-      case Some(a) => σ(a)
+    case ANFIdentifier(name) => {
+      println(s"Evaluating identifier $name")
+      ρ(name) match {
+      case Some(a) => {
+        println(s"Addr: $a")
+        σ(a)
+      }
       case None => throw new Exception(s"Unbound variable: $name")
+      }
     }
     case ANFValue(value) => Set(AbstractSimpleValue(value))
   }
 
-  def stepEval(e: ANFExp, ρ: Env, σ: Store, κ: Kont): Set[State] = e match {
+  def stepEval(e: ANFExp, ρ: Env, σ: Store, κ: KontAddress): Set[State] = e match {
     case ae: ANFAtomicExp => reachedValue(atomicEval(ae, ρ, σ), σ, κ)
     case ANFIf(cond, cons, alt) =>
       atomicEval(cond, ρ, σ).foldLeft(Set[State]())((s: Set[State], v: AbstractValue) => {
@@ -140,13 +162,28 @@ case class State(control: Control, σ: Store, κ: Kont) {
           s
         }
       })
+    case ANFLet(variable, value, body) =>
+      push(value, KontLet(variable, body, ρ, κ), ρ, σ)
   }
 
   def stepKont(v: AbstractValue, σ: Store, κ: Kont): Set[State] = κ match {
     case KontHalt() => Set(this)
+    case KontLet(variable, body, ρ, addr) => {
+      println(s"Allocating $v")
+      val a = allocVariable(variable)
+      Set(State(ControlEval(body, ρ + (variable -> a)), σ ⊔ (a -> v), addr))
+    }
   }
 
-  def reachedValue(vs: Set[AbstractValue], σ: Store, κ: Kont): Set[State] =
+  def push(e: ANFExp, κ: Kont,  ρ: Env, σ: Store): Set[State] = {
+    val a = allocKont(e, κ)
+    Set(State(ControlEval(e, ρ), σ ⊔ (a -> AbstractKont(κ)), a))
+  }
+
+  def allocKont(e: ANFExp, κ: Kont): KontAddress = KontAddress(e)
+  def allocVariable(variable: String): VariableAddress = VariableAddress(variable)
+
+  def reachedValue(vs: Set[AbstractValue], σ: Store, κ: KontAddress): Set[State] =
     vs.foldLeft(Set[State]())((s, v) => s + State(ControlKont(v), σ, κ))
 }
 
@@ -158,6 +195,7 @@ object AAM {
       } else if (s.halted) {
         loop(todo.tail, visited + s, halted + s)
       } else {
+        println("Stepping " + s)
         loop(todo.tail ++ s.step, visited + s, halted)
       }
     case None => halted
