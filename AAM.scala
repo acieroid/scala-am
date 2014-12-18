1411fce10d89a8c7082b6fb736777ea2d8cd8c9c
Quentin Stievenart <quentin.stievenart@gmail.com>
2014-12-18 12:03:15 +0100

    Refactoring

--- AAM.scala
+++ AAM.scala
@@ -2,6 +2,8 @@
   * Implementation of a CESK machine for ANF following the AAM approach
   */
 
+import AbstractValue._
+
 object Options {
   var debug = false
 }
@@ -68,208 +70,6 @@ case class KontLetrec(v: String, a: Address, body: ANFExp, env: Env, next: KontA
 object KontHalt extends Kont {
   override def toString(): String = s"KontHalt"
 }
-
-sealed abstract class AbstractValue {
-  def isTrue: Boolean
-  def isFalse: Boolean = !isTrue
-  /** Fold a function over the values contained in this abstract value. This
-      should be redefined only for container-like abstract values. */
-  def foldValues[A](f: AbstractValue => Set[A]): Set[A] = f(this)
-  def ⊔(x: AbstractValue): AbstractValue = if (this.equals(x)) { this } else { x match {
-    case v: AbstractValueSet => v ⊔ x /* TODO: is there a better solution than hardcoding this here? */
-    case _ => AbstractValueSet(Set(this, x))
-  }}
-  def ⊓(x: AbstractValue): AbstractValue = if (this.equals(x)) { this } else { AbstractBottom() }
-  def ⊒(x: AbstractValue): Boolean
-
-  /* Primitive operations */
-  def +(x: AbstractValue): AbstractValue = AbstractBottom()
-  def -(x: AbstractValue): AbstractValue = AbstractBottom()
-  def *(x: AbstractValue): AbstractValue = AbstractBottom()
-  def <(x: AbstractValue): AbstractValue = AbstractBottom()
-  def <=(x: AbstractValue): AbstractValue = AbstractBottom()
-  def >(x: AbstractValue): AbstractValue = AbstractBottom()
-  def >=(x: AbstractValue): AbstractValue = AbstractBottom()
-  def absEq(x: AbstractValue): AbstractValue = AbstractBottom()
-  def unary_!(): AbstractValue = AbstractBottom()
-}
-object AbstractBottom {
-  def apply() = AbstractValueSet(Set())
-  def unapply(x: AbstractValue) = x match {
-    case AbstractValueSet(set) => set.size == 0
-    case _ => false
-  }
-  def ⊒(x: AbstractValue) = false
-}
-case class AbstractClosure(λ: ANFLambda, ρ: Env) extends AbstractValue {
-  override def toString: String = s"#<clo>"
-  def isTrue = true
-  def ⊒(x: AbstractValue) = x match {
-    case AbstractClosure(_, _) => x == this
-    case AbstractBottom() => true
-    case _ => false
-  }
-}
-case class AbstractPrimitive(name: String, f: List[AbstractValue] => AbstractValue) extends AbstractValue {
-  override def toString: String = s"#<prim $name>"
-  def isTrue = true
-  def ⊒(x: AbstractValue) = x match {
-    case AbstractPrimitive(n, _) => name == n
-    case _ => false
-  }
-}
-case class AbstractKont(κ: Kont) extends AbstractValue {
-  override def toString: String = s"#<kont $κ>"
-  def isTrue = false
-  override def isFalse = false
-  def ⊒(x: AbstractValue) = x match {
-    case AbstractKont(κ2) => κ == κ2
-    case AbstractBottom() => true
-    case _ => false
-  }
-}
-case class AbstractSimpleValue(value: Value) extends AbstractValue {
-  override def toString: String = value.toString
-  def isTrue = value match {
-    case ValueBoolean(false) => false
-    case _ => true
-  }
-  def ⊒(x: AbstractValue) = x match {
-    case AbstractSimpleValue(v) => value == v
-    case AbstractBottom() => true
-    case _ => false
-  }
-
-  override def ⊔(x: AbstractValue): AbstractValue = (value, x) match {
-    case (ValueInteger(a), AbstractSimpleValue(ValueInteger(b))) if a != b => AbstractInt
-    case (ValueInteger(_), AbstractInt) => AbstractInt
-    case _ => super.⊔(x)
-  }
-
-  def numOp(f: (Integer, Integer) => Integer): (Value, AbstractValue) => AbstractValue = {
-    case (ValueInteger(a), AbstractSimpleValue(ValueInteger(b))) => AbstractSimpleValue(ValueInteger(f(a,b)))
-    case (x, AbstractValueSet(s)) => s.foldLeft(AbstractValueSet())((acc, y) => acc ⊔ numOp(f)(x,y))
-    case (x, AbstractInt) => AbstractInt
-    case _ => AbstractBottom()
-  }
-
-  def cmpOp(f: (Integer, Integer) => Boolean): (Value, AbstractValue) => AbstractValue = {
-    case (ValueInteger(a), AbstractSimpleValue(ValueInteger(b))) => AbstractSimpleValue(ValueBoolean(f(a,b)))
-    case (x, AbstractValueSet(s)) => s.foldLeft(AbstractValueSet())((acc, y) => acc ⊔ cmpOp(f)(x,y))
-    case (x, AbstractInt) => AbstractBool()
-    case _ => AbstractBottom()
-  }
-
-  override def +(x: AbstractValue) = numOp((a, b) => a + b)(value, x)
-  override def -(x: AbstractValue) = numOp((a, b) => a - b)(value, x)
-  override def *(x: AbstractValue) = numOp((a, b) => a * b)(value, x)
-  override def <(x: AbstractValue) = cmpOp((a, b) => a < b)(value, x)
-  override def <=(x: AbstractValue) = cmpOp((a, b) => a <= b)(value, x)
-  override def >(x: AbstractValue) = cmpOp((a, b) => a > b)(value, x)
-  override def >=(x: AbstractValue) = cmpOp((a, b) => a >= b)(value, x)
-  override def absEq(x: AbstractValue) = cmpOp((a, b) => a == b)(value, x)
-  override def unary_!() = value match {
-    case ValueBoolean(false) => AbstractSimpleValue(ValueBoolean(true))
-    case _ => AbstractSimpleValue(ValueBoolean(false))
-  }
-}
-object AbstractInt extends AbstractValue {
-  override def toString: String = "Int"
-  def isTrue = true
-  def ⊒(x: AbstractValue) = x match {
-    case AbstractBottom() => true
-    case AbstractInt => true
-    case AbstractSimpleValue(ValueInteger(_)) => true
-    case AbstractValueSet(set) => set.foldLeft(true)((acc, v) => acc && this ⊒ v)
-    case _ => false
-  }
-
-  override def ⊔(x: AbstractValue): AbstractValue = x match {
-    case AbstractSimpleValue(ValueInteger(_)) => AbstractInt
-    case _ => super.⊔(x)
-  }
-
-  def numOp(x: AbstractValue): AbstractValue = x match {
-    case AbstractInt => AbstractInt
-    case AbstractSimpleValue(ValueInteger(_)) => AbstractInt
-    case AbstractValueSet(s) => s.foldLeft(AbstractValueSet())((acc, y) => acc ⊔ numOp(y))
-    case _ => AbstractBottom()
-  }
-
-  def cmpOp(x: AbstractValue): AbstractValue = x match {
-    case AbstractInt => AbstractBool()
-    case AbstractSimpleValue(ValueInteger(_)) => AbstractBool()
-    case AbstractValueSet(s) => s.foldLeft(AbstractValueSet())((acc, y) => acc ⊔ cmpOp(y))
-    case _ => AbstractBottom()
-  }
-
-  override def +(x: AbstractValue) = numOp(x)
-  override def -(x: AbstractValue) = numOp(x)
-  override def *(x: AbstractValue) = numOp(x)
-  override def <(x: AbstractValue) = cmpOp(x)
-  override def <=(x: AbstractValue) = cmpOp(x)
-  override def >(x: AbstractValue) = cmpOp(x)
-  override def >=(x: AbstractValue) = cmpOp(x)
-  override def absEq(x: AbstractValue) = cmpOp(x)
-  override def unary_!() = AbstractSimpleValue(ValueBoolean(false))
-}
-object AbstractBool {
-  def apply(): AbstractValue = AbstractValueSet(Set(AbstractSimpleValue(ValueBoolean(true)),
-                                                    AbstractSimpleValue(ValueBoolean(false))))
-}
-case class AbstractValueSet(values: Set[AbstractValue]) extends AbstractValue {
-  override def toString(): String = "{" + values.mkString(", ") + "}"
-  def isTrue = values.exists(_.isTrue)
-  override def isFalse = values.exists(_.isFalse)
-  def ⊒(x: AbstractValue) = x match {
-    case AbstractValueSet(set) => set.foldLeft(true)((acc, v) => this ⊒ v)
-    case _ => values.exists(v => v ⊒ x)
-  }
-
-  override def foldValues[A](f: AbstractValue => Set[A]): Set[A] =
-    values.foldLeft(Set[A]())((s: Set[A], v: AbstractValue) => s ++ v.foldValues(f))
-  /** The join result will remain a set of values */
-  override def ⊔(x: AbstractValue): AbstractValueSet = x match {
-    case AbstractValueSet(set) =>
-      if (set.size == 0) { this } else if (values.size == 0) { AbstractValueSet(set) } else {
-        AbstractValueSet(values.foldLeft(Set[AbstractValue]())((acc, v) => {
-          /* Tries to join v with an item of set such that the resulting element is not another set */
-          set.foldLeft((Set[AbstractValue](), false))({
-            case ((acc2, true), v2) => (acc2 + v2, true)
-            case ((acc2, false), v2) => v ⊔ v2 match {
-              case AbstractValueSet(_) => (acc2 + v2, false)
-              case joined => (acc2 + joined, true)
-            }
-          }) match {
-            case (s, true) => acc ++ s
-            case (s, false) => acc ++ s + v /* if it did not succeed, just add v to the set as is */
-          }
-        }))
-      }
-    case _ => this ⊔ AbstractValueSet(Set(x))
-  }
-  override def ⊓(x: AbstractValue): AbstractValue = x match {
-    case AbstractValueSet(s) => AbstractValueSet(s.intersect(values))
-    case _ => if (values.contains(x)) { x } else { AbstractBottom() }
-  }
-  def op(f: (AbstractValue, AbstractValue) => AbstractValue): (Set[AbstractValue], AbstractValue) => AbstractValue = {
-    case (s, x) => s.foldLeft(AbstractValueSet())((acc, v) => acc ⊔ (f(v, x)))
-  }
-  override def +(x: AbstractValue) = op((a, b) => a + b)(values, x)
-  override def -(x: AbstractValue) = op((a, b) => a - b)(values, x)
-  override def *(x: AbstractValue) = op((a, b) => a * b)(values, x)
-  override def <(x: AbstractValue) = op((a, b) => a < b)(values, x)
-  override def <=(x: AbstractValue) = op((a, b) => a <= b)(values, x)
-  override def >(x: AbstractValue) = op((a, b) => a > b)(values, x)
-  override def >=(x: AbstractValue) = op((a, b) => a >= b)(values, x)
-  override def absEq(x: AbstractValue) = op((a, b) => a absEq b)(values, x)
-  override def unary_!() = values.foldLeft(AbstractValueSet())((acc, v) => acc ⊔ !v)
-}
-object AbstractValueSet {
-  def apply(): AbstractValueSet = AbstractValueSet(Set[AbstractValue]())
-}
-
-/* TODO: abstract pairs */
 
 sealed abstract class Control {
   def ⊒(x: Control): Boolean
@@ -452,49 +252,18 @@ case class State(control: Control, σ: Store, a: KontAddress) {
   def allocVariable(variable: String, σ: Store): VariableAddress = VariableAddress(variable, 0)
 }
 
-/** TODO: parameterize */
-case class Graph(ids: Map[State, Int], next: Int, nodes: Set[State], edges: Map[State, Set[State]]) {
-  def this() = this(Map[State, Int](), 0, Set[State](), Map[State, Set[State]]())
-  def this(state: State) = this(Map[State, Int]() + (state -> 0), 1, Set[State](state), Map[State, Set[State]]())
-  def addNode(node: State): Graph =
-    if (nodes.contains(node)) { this } else {
-      Graph(ids + (node -> next), next + 1, nodes + node, edges)
-    }
-  def addEdge(node1: State, node2: State): Graph =
-      addNode(node1).addNode(node2).addEdgeNoCheck(node1, node2)
-  def addEdges(l: Traversable[(State, State)]): Graph =
-    l.foldLeft(this)({ case (g, (n1, n2)) => g.addEdge(n1, n2) })
-  def addEdgeNoCheck(node1: State, node2: State): Graph =
-    if (edges.contains(node1) && edges(node1).contains(node2)) { this } else {
-      val existing: Set[State] = edges.getOrElse(node1, Set[State]())
-      Graph(ids, next, nodes, edges + (node1 -> (existing ++ Set(node2))))
-    }
-  def size(): Integer = nodes.size
-  def toDot(): String = {
-      val sb = new StringBuilder("digraph G {\n")
-      nodes.foreach((n) => {
-        val color = n.control match {
+object AAM {
+  def outputDot(graph: Graph[State], path: String) = {
+    graph.toDotFile(path, _.toString.take(40), _.control match {
           case ControlEval(_, _) => "#DDFFDD"
           case ControlKont(_) => "#FFDDDD"
-        }
-        sb.append("node_" + ids(n) + "[label=\"" + n.toString.take(40).replaceAll("\"", "\\\\\"") + "\", fillcolor=\"" + color + "\" style=\"filled\"];\n")
-      })
-      edges.foreach({ case (n1, ns) => ns.foreach((n2) => sb.append(s"node_${ids(n1)} -> node_${ids(n2)}\n")) })
-      sb.append("}")
-      return sb.toString
-    }
-}
-
-object AAM {
-  def outputDot(graph: Graph, path: String) = {
-    val dot = graph.toDot()
-    val f = new java.io.File(path)
-    val bw = new java.io.BufferedWriter(new java.io.FileWriter(f))
-    bw.write(dot)
-    bw.close()
+    })
   }
 
-  def loop(todo: Set[State], visited: Set[State], halted: Set[State], graph: Graph): (Set[State], Graph) = todo.headOption match {
+  /** Explore the state space by calling the step function until it reaches a
+      final state. Returns the set of final state as well as the constructed
+      graph */
+  def loop(todo: Set[State], visited: Set[State], halted: Set[State], graph: Graph[State]): (Set[State], Graph[State]) = todo.headOption match {
     case Some(s) =>
       if (visited.contains(s) || visited.exists(s2 => s2 ⊒ s)) {
         /*
@@ -528,8 +297,8 @@ object AAM {
 
   def eval(exp: ANFExp) = {
     val state = new State(exp)
-    loop(Set(state), Set(), Set(), new Graph(state)) match {
-      case (halted, graph: Graph) => {
+    loop(Set(state), Set(), Set(), new Graph[State](state)) match {
+      case (halted, graph: Graph[State]) => {
         println(s"${graph.size} states")
         outputDot(graph, "foo.dot")
         halted
---
+++ Graph.scala
@@ -0,0 +1,33 @@
+case class Graph[A](ids: Map[A, Int], next: Int, nodes: Set[A], edges: Map[A, Set[A]]) {
+  def this() = this(Map[A, Int](), 0, Set[A](), Map[A, Set[A]]())
+  def this(node: A) = this(Map[A, Int]() + (node -> 0), 1, Set[A](node), Map[A, Set[A]]())
+  def addNode(node: A): Graph[A] =
+    if (nodes.contains(node)) { this } else {
+      Graph(ids + (node -> next), next + 1, nodes + node, edges)
+    }
+  def addEdge(node1: A, node2: A): Graph[A] =
+      addNode(node1).addNode(node2).addEdgeNoCheck(node1, node2)
+  def addEdges(l: Traversable[(A, A)]): Graph[A] =
+    l.foldLeft(this)({ case (g, (n1, n2)) => g.addEdge(n1, n2) })
+  def addEdgeNoCheck(node1: A, node2: A): Graph[A] =
+    if (edges.contains(node1) && edges(node1).contains(node2)) { this } else {
+      val existing: Set[A] = edges.getOrElse(node1, Set[A]())
+      Graph(ids, next, nodes, edges + (node1 -> (existing ++ Set(node2))))
+    }
+  def size(): Integer = nodes.size
+  def toDot(label: A => String, color: A => String): String = {
+      val sb = new StringBuilder("digraph G {\n")
+      nodes.foreach((n) =>
+        sb.append("node_" + ids(n) + "[label=\"" + label(n).replaceAll("\"", "\\\\\"") + "\", fillcolor=\"" + color(n) + "\" style=\"filled\"];\n")
+      )
+      edges.foreach({ case (n1, ns) => ns.foreach((n2) => sb.append(s"node_${ids(n1)} -> node_${ids(n2)}\n")) })
+      sb.append("}")
+      return sb.toString
+    }
+  def toDotFile(path: String, label: A => String, color: A => String): Unit = {
+    val f = new java.io.File(path)
+    val bw = new java.io.BufferedWriter(new java.io.FileWriter(f))
+    bw.write(toDot(label, color))
+    bw.close()
+  }
+}
