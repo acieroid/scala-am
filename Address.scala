a1b534866fc6d934fadc2b4a0d81b9e58c8a0f5c
Quentin Stievenart <quentin.stievenart@gmail.com>
2015-08-12 16:33:25 +0200

    Add concrete addresses

--- Address.scala
+++ Address.scala
@@ -12,13 +12,14 @@ trait AddressInjection[A] {
 trait ClassicalAddress {
   def subsumes(that: ClassicalAddress) = this.equals(that)
 }
-case class VariableAddress(name: String) extends ClassicalAddress
-case class PrimitiveAddress(name: String) extends ClassicalAddress
-abstract class KontAddress extends ClassicalAddress
-case class NormalKontAddress[Exp : Expression](exp: Exp) extends KontAddress
-case class HaltKontAddress() extends KontAddress
 
-object Address {
+object ClassicalAddress {
+  case class VariableAddress(name: String) extends ClassicalAddress
+  case class PrimitiveAddress(name: String) extends ClassicalAddress
+  abstract class KontAddress extends ClassicalAddress
+  case class NormalKontAddress[Exp : Expression](exp: Exp) extends KontAddress
+  case class HaltKontAddress() extends KontAddress
+
   implicit object ClassicalAddressAddress extends Address[ClassicalAddress] {
     override def subsumes(x: ClassicalAddress, y: ClassicalAddress) = x.subsumes(y)
   }
@@ -27,5 +28,23 @@ object Address {
     def primitive(name: String) = PrimitiveAddress(name)
     def variable(name: String) = VariableAddress(name)
     def kont[Exp : Expression](exp: Exp) = NormalKontAddress(exp)
+  }
+}
+
+trait ConcreteAddress {
+  def subsumes(that: ConcreteAddress) = this.equals(that)
+}
+
+object ConcreteAddress {
+  case class IntAddress(name: String, id: Int) extends ConcreteAddress
+  implicit object ConcreteAddressAddress extends Address[ConcreteAddress] {
+    override def subsumes(x: ConcreteAddress, y: ConcreteAddress) = x.subsumes(y)
+  }
+  var id = 0
+  implicit object ConcreteAddressInjection extends AddressInjection[ConcreteAddress] {
+    def halt = IntAddress("halt", 0)
+    def primitive(name: String) = { id += 1; IntAddress(name, id) }
+    def variable(name: String) = { id += 1; IntAddress(name, id) }
+    def kont[Exp : Expression](exp: Exp) = { id += 1; IntAddress(s"kont-$exp", id) }
   }
 }
