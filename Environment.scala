4bc82b1766ea6738f841fd72a29444bd19009376
Quentin Stievenart <quentin.stievenart@gmail.com>
2015-08-17 16:35:19 +0200

    Don't print primitives in environments

--- Environment.scala
+++ Environment.scala
@@ -1,12 +1,13 @@
-case class Environment[Addr : Address](content: Map[String, Addr]) {
+case class Environment[Addr](content: Map[String, Addr])(implicit addr: Address[Addr]) {
+  override def toString = content.filter({ case (_, a) => !addr.isPrimitive(a) }).toString
   def keys = content.keys
   def forall(p: ((String, Addr)) => Boolean) = content.forall(p)
   def lookup(name: String) = content.get(name)
-  def extend(name: String, addr: Addr) = Environment(content + (name -> addr))
+  def extend(name: String, a: Addr) = Environment(content + (name -> a))
   def extend(values: List[(String, Addr)]) = Environment(content ++ values)
   def subsumes(that: Environment[Addr]) =
     that.forall((binding: (String, Addr)) => lookup(binding._1) match {
-      case Some(addr) => implicitly[Address[Addr]].subsumes(addr, binding._2)
+      case Some(a) => addr.subsumes(a, binding._2)
       case None => false
     })
 }
