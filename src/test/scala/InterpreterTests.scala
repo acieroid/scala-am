bff0d58ba8a78fc7cb7d1d4428d761d9e8d48816
Quentin Stievenart <quentin.stievenart@gmail.com>
2015-10-01 13:24:33 +0200

    Factor interpreter tests

--- src/test/scala/InterpreterTests.scala
+++ src/test/scala/InterpreterTests.scala
@@ -5,26 +5,29 @@ abstract class Benchmarks[Abs, Addr](implicit abs: AbstractValue[Abs], absi: Abs
   addr: Address[Addr], addri: AddressInjection[Addr])
     extends FlatSpec with Matchers {
   def checkResult(file: String, expected: Abs): Unit
+  def check(file: String, expected: Abs) =
+    file should s"eval to $expected" in { checkResult(file, expected) }
 
-  "blur.scm" should "eval to #t" in { checkResult("blur.scm", absi.inject(true)) }
-  "count.scm" should "eval to \"done\"" in { checkResult("count.scm", absi.inject("done")) }
-  "cpstak.scm" should "eval to 6" in { checkResult("cpstak.scm", absi.inject(6)) }
-  "fib.scm" should "eval to 3" in { checkResult("fib.scm", absi.inject(3)) }
-  "eta.scm" should "eval to #f" in { checkResult("eta.scm", absi.inject(false)) }
-  "fact.scm" should "eval to 120" in { checkResult("fact.scm", absi.inject(120)) }
-  "gcipd.scm" should "eval to 36" in { checkResult("gcipd.scm", absi.inject(36)) }
-  "inc.scm" should "eval to 4" in { checkResult("inc.scm", absi.inject(4)) }
-  "kcfa2.scm" should "eval to #f" in { checkResult("kcfa2.scm", absi.inject(false)) }
-  "kcfa3.scm" should "eval to #f" in { checkResult("kcfa3.scm", absi.inject(false)) }
-  "loop2.scm" should "eval to 550" in { checkResult("loop2.scm", absi.inject(550)) }
-  "mj09.scm" should "eval to 2" in { checkResult("mj09.scm", absi.inject(2)) }
-  "mut-rec.scm" should "eval to #t" in { checkResult("mut-rec.scm", absi.inject(true)) }
-  "rotate.scm" should "eval to \"hallo\"" in { checkResult("rotate.scm", absi.inject("hallo")) }
-  "sq.scm" should "eval to 9" in { checkResult("sq.scm", absi.inject(9)) }
-  "sym.scm" should "eval to 'foo" in { checkResult("sym.scm", absi.injectSymbol("foo")) }
-  "rsa.scm" should "eval to #t" in { checkResult("rsa.scm", absi.inject(true)) }
-  "sat.scm" should "eval to #t" in { checkResult("sat.scm", absi.inject(true)) }
-  "primtest.scm" should "eval to 1" in { checkResult("primtest.scm", absi.inject(1)) }
+  check("blur.scm", absi.inject(true))
+  check("count.scm", absi.inject("done"))
+  check("cpstak.scm", absi.inject(6))
+  check("fib.scm", absi.inject(3))
+  check("eta.scm", absi.inject(false))
+  check("fact.scm", absi.inject(120))
+  check("gcipd.scm", absi.inject(36))
+  check("inc.scm", absi.inject(4))
+  check("kcfa2.scm", absi.inject(false))
+  check("kcfa3.scm", absi.inject(false))
+  check("loop2.scm", absi.inject(550))
+  check("mj09.scm", absi.inject(2))
+  check("mut-rec.scm", absi.inject(true))
+  check("rotate.scm", absi.inject("hallo"))
+  check("sq.scm", absi.inject(9))
+  check("sym.scm", absi.injectSymbol("foo"))
+  check("rsa.scm", absi.inject(true))
+  check("sat.scm", absi.inject(true))
+  check("primtest.scm", absi.inject(1))
+  check("nqueens.scm", absi.inject(92))
 }
 
 abstract class AACBenchmarks[Abs, Addr](implicit abs: AbstractValue[Abs], absi: AbstractInjection[Abs],
