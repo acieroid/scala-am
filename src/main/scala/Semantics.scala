a97bbb87ec53a9bcbaa0521451595dcb8cef39a6
Quentin Stievenart <quentin.stievenart@gmail.com>
2015-10-05 16:30:09 +0200

    Add documentation in Semantics.scala

--- src/main/scala/Semantics.scala
+++ src/main/scala/Semantics.scala
@@ -1,18 +1,63 @@
+/**
+ * This is where the semantics of a language are defined. By defining the
+ * semantics of a language, you get an abstract abstract machine for free (but
+ * you might need to adapt existing lattices to support values from your
+ * language).
+ *
+ * Semantics should be defined as small-step operational semantics. To define a
+ * semantics, you have to implement the Semantics trait. You'll need to
+ * specialize it on the type of expression of your language (e.g., for ANF,
+ * ANFSemantics specializes on ANFExp). To do so, you need to define what
+ * actions should be taken when:
+ *   1. Evaluating an expression e (stepEval)
+ *   2. Continuing evaluation when a value v has been reached (stepKont)
+ *
+ * To have a simple overview of how semantics should be defined, look at the
+ * ANFSemantics class, as it defines semantics of ANF Scheme, a very lightweight
+ * language.
+ */
+
 trait Semantics[Exp, Abs, Addr] {
   implicit def abs : AbstractValue[Abs]
   implicit def absi : AbstractInjection[Abs]
   implicit def addr : Address[Addr]
   implicit def addri : AddressInjection[Addr]
   implicit def exp : Expression[Exp]
+  /**
+   * Defines what actions should be taken when an expression e needs to be
+   * evaluated, in environment e with store σ
+   */
   def stepEval(e: Exp, ρ: Environment[Addr], σ: Store[Addr, Abs]): Set[Action[Exp, Abs, Addr]]
+  /**
+   * Defines what actions should be taken when a value v has been reached, and
+   * the topmost frame is frame
+   */
   def stepKont(v: Abs, σ: Store[Addr, Abs], frame: Frame): Set[Action[Exp, Abs, Addr]]
 }
 
+/**
+ * The different kinds of actions that can be taken by the abstract machine
+ */
 abstract class Action[Exp : Expression, Abs : AbstractValue, Addr : Address]
+/**
+ * A value is reached by the interpreter. As a result, a continuation will be popped with the given reached value.
+ */
 case class ActionReachedValue[Exp : Expression, Abs : AbstractValue, Addr : Address](v: Abs, σ: Store[Addr, Abs]) extends Action[Exp, Abs, Addr]
+/**
+ * A frame needs to be pushed on the stack, and the interpretation continues by evaluating expression e in environment ρ
+ */
 case class ActionPush[Exp : Expression, Abs : AbstractValue, Addr : Address](e: Exp, frame: Frame, ρ: Environment[Addr], σ: Store[Addr, Abs]) extends Action[Exp, Abs, Addr]
+/**
+ * Evaluation continues with expression e in environment ρ
+ */
 case class ActionEval[Exp : Expression, Abs : AbstractValue, Addr : Address](e: Exp, ρ: Environment[Addr], σ: Store[Addr, Abs]) extends Action[Exp, Abs, Addr]
+/**
+ * Similar to ActionEval, but only used when stepping inside a function's body (clo is therefore the function stepped into)
+ */
 case class ActionStepIn[Exp : Expression, Abs : AbstractValue, Addr : Address](clo: (Exp, Environment[Addr]), e: Exp, ρ: Environment[Addr], σ: Store[Addr, Abs]) extends Action[Exp, Abs, Addr]
+/**
+ * An error has been reached
+ */
 case class ActionError[Exp : Expression, Abs : AbstractValue, Addr : Address](reason: String) extends Action[Exp, Abs, Addr]
 
 class ANFSemantics[Abs, Addr](implicit ab: AbstractValue[Abs], abi: AbstractInjection[Abs],
