4260048258295bb809df292609580ed85361e258
Quentin Stievenart <quentin.stievenart@gmail.com>
2015-10-12 09:31:56 +0200

    Update Main.scala's documentation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,33 +3,39 @@ import scala.io.StdIn
 
 /**
  * This is the entry point. It parses the arguments, parses the input file and
- * launch an abstract machine on the parsed expression. The code isn't very
- * clean and I'd like to improve it at some point, but my scala-fu isn't good
- * enough to do it now. The pipeline goes as follows:
- *   1. The input file is parsed. For Scheme files, it is done by:
+ * launch an abstract machine on the parsed expression (or launches a REPL if no
+ * input file is given). The code in this file isn't very clean and I'd like to
+ * improve it at some point, but my scala-fu isn't good enough to do it now. The
+ * pipeline goes as follows:
+ *   1. The input program is parsed. For Scheme programs, it is done by:
  *      - Parsing the file as a list of s-expressions (SExp.scala, SExpParser.scala)
  *      - Compiling these s-expressions into Scheme expressions (Scheme.scala)
  *      - Optionally, converting Scheme expressions into ANF form (ANF.scala) to
  *        have a simpler interpreter (but longer programs)
 
- *   2. The abstract machine is created by giving it some semantics. For now,
- *      the only semantics available are ANF Scheme semantics and Scheme
- *      semantics (ANFSemantics.scala, SchemeSemantics.scala)
+ *   2. To run the program, we need an abstract machine and some semantics. For
+ *      now, the only semantics available are for ANF Scheme and Scheme
+ *      (ANFSemantics.scala, SchemeSemantics.scala). Semantics definitions have
+ *      to implement the Semantics interface (Semantics.scala).
 
- *   3. The abstract machine performs its evaluation, relying on methods of the
- *      semantics class to know how to evaluate expressions. The abstract
- *      machine only deals with which states to evaluate in which order, where
- *      to store values, where to store continuations, how to push and pop
- *      continuations, etc. The semantics encode what to do when encountering a
- *      program construct. For example, the semantics can tell what to evaluate
- *      next, that a continuation needs to be pushed, or that a variable needs
- *      to be updated. The abstract machine will then respectively evaluate the
- *      expression needed, push the continuation, or update the variable.
+ *   3. Once the abstract machine is created and we have a semantics for the
+ *      program we want to analyze, the abstract machine can perform its
+ *      evaluation, relying on methods of the semantics class to know how to
+ *      evaluate expressions. The abstract machine only deals with which states
+ *      to evaluate in which order, where to store values, where to store
+ *      continuations, how to push and pop continuations, etc. The semantics
+ *      encode what to do when encountering a program construct. For example,
+ *      the semantics can tell what to evaluate next, that a continuation needs
+ *      to be pushed, or that a variable needs to be updated. The abstract
+ *      machine will then respectively evaluate the expression needed, push the
+ *      continuation, or update the variable.
  *
  *      Three abstract machine implementations are available:
  *      - The classical Abstracting Abstract Machine of Might and Van Horn (AAM.scala)
  *      - Johnson's Abstracting Abstract Control (AAC.scala)
- *      - Gilrey's Pushdown Control-Flow Analysis for Free (Free.scala)
+ *      - Gilrey's Pushdown Control-Flow Analysis for Free (Free.scala) Every
+ *      abstract machine implementation has to implement the AbstractMachine
+ *      interface (AbstractMachine.scala).
  *
  *      The abstract machine also uses a lattice to represent values. Lattices
  *      should implement some traits that can be found in
@@ -50,12 +56,17 @@ import scala.io.StdIn
  *    - Support for your language operations at the lattice level. For this,
  *      you'll probably need to extend the lattices (AbstractValue.scala,
  *      AbstractConcrete.scala, AbstractType.scala, AbstractTypeSet.scala)
- *  - Play with abstract machines, you can look into AAM.scala, AAC.scala or Free.scala
+ *  - Play with abstract machines, you can look into AAM.scala, AAC.scala or
+ *    Free.scala (AAM is the simplest machine).
  *  - Implement some kind of analysis, you'll probably need to design a lattice
  *    that is suited for your analysis. You can use an existing lattice as an
  *    inspiration (AbstractType.scala is a good lattice to start with, even
- *    though its very imprecise, it's simple. AbstractTypeSet.scala for
- *    example).
+ *    though its very imprecise, it's simple. You can then look into
+ *    AbstractTypeSet.scala).
+ */
+
+/**
+ * This is where we parse the arguments given to the implementation
  */
 object Config {
   object Machine extends Enumeration {
@@ -86,6 +97,9 @@ object Config {
 
 object Main {
 
+  /** Run a machine on a program with the given semantics. If @param output is
+    * set, generate a dot graph visualizing the computed graph in the given
+    * file. */
   def run[Exp, Abs, Addr](machine: AbstractMachine[Exp, Abs, Addr], sem: Semantics[Exp, Abs, Addr])(program: String, output: Option[String])
   (implicit abs: AbstractValue[Abs], absi: AbstractInjection[Abs],
     addr: Address[Addr], addri: AddressInjection[Addr]): Unit = {
