9bd0da1445be73184c209c7cd443c0ee05be5aaf
Quentin Stievenart <quentin.stievenart@gmail.com>
2015-12-15 13:44:35 +0100

    Add a powerset lattice and a type lattice with #t,#f

---
+++ src/main/scala/PowerSetLattice.scala
@@ -0,0 +1,134 @@
+import UnaryOperator._
+import BinaryOperator._
+
+/**
+ * Transform a lattice of elements into a lattice of sets of elements
+ */
+class PowerSetLattice[X : AbstractValue] {
+  val abs = implicitly[AbstractValue[X]]
+
+  trait PowerSet
+  case class Element(x: X) extends PowerSet {
+    override def toString = x.toString
+  }
+  case class Elements(xs: Set[X]) extends PowerSet {
+    require(xs.size > 0, "PowerSet element created with no elements inside, X's bottom should be used instead")
+    override def toString = "{" + xs.mkString(", ") + "}"
+  }
+
+  implicit object PowerSetAbstractValue extends AbstractValue[PowerSet] {
+    def name = "PowerSet(${abs.name})"
+    def isTrue(p: PowerSet) = p match {
+      case Element(x) => abs.isTrue(x)
+      case Elements(xs) => xs.exists(x => abs.isTrue(x))
+    }
+    def isFalse(p: PowerSet) = p match {
+      case Element(x) => abs.isFalse(x)
+      case Elements(xs) => xs.exists(x => abs.isFalse(x))
+    }
+    def isError(p: PowerSet) = p match {
+      case Element(x) => abs.isError(x)
+      case Elements(xs) => {
+        val result = xs.exists(x => abs.isError(x))
+        require(!result || xs.forall(x => abs.isError(x)), s"PowerSet element combines error values with non-error values ($xs)")
+        result
+      }
+    }
+    private def wrapped(x: => X): PowerSet = try {
+      Element(x)
+    } catch {
+      case CannotJoin(x1: X, x2: X) => Elements(Set[X](x1, x2))
+    }
+    def unaryOp(op: UnaryOperator)(p: PowerSet) = p match {
+      case Element(x) => wrapped(abs.unaryOp(op)(x))
+      case Elements(xs) => xs.foldLeft(bottom)((acc, x) => join(acc, wrapped(abs.unaryOp(op)(x))))
+    }
+    def binaryOp(op: BinaryOperator)(p1: PowerSet, p2: PowerSet) = (p1, p2) match {
+      case (Element(x1), Element(x2)) => wrapped(abs.binaryOp(op)(x1, x2))
+      case (Element(x), Elements(xs)) => binaryOp(op)(Elements(Set[X](x)), p2)
+      case (Elements(xs), Element(x)) => binaryOp(op)(p1, Elements(Set[X](x)))
+      case (Elements(xs1), Elements(xs2)) =>
+        xs1.foldLeft(bottom)((acc, x1) => xs2.foldLeft(acc)((acc, x2) => join(acc, wrapped(abs.binaryOp(op)(x1, x2)))))
+    }
+    def and(p1: PowerSet, p2: => PowerSet) = (p1, p2) match {
+      case (Element(x1), Element(x2)) => wrapped(abs.and(x1, x2))
+      case (Element(x), Elements(xs)) => and(Elements(Set[X](x)), p2)
+      case (Elements(xs), Element(x)) => and(p1, Elements(Set[X](x)))
+      case (Elements(xs1), Elements(xs2)) =>
+        xs1.foldLeft(bottom)((acc, x1) => xs2.foldLeft(acc)((acc, x2) => join(acc, wrapped(abs.and(x1, x2)))))
+    }
+    def or(p1: PowerSet, p2: => PowerSet) = (p1, p2) match {
+      case (Element(x1), Element(x2)) => wrapped(abs.or(x1, x2))
+      case (Element(x), Elements(xs)) => or(Elements(Set[X](x)), p2)
+      case (Elements(xs), Element(x)) => or(p1, Elements(Set[X](x)))
+      case (Elements(xs1), Elements(xs2)) => ???
+        xs1.foldLeft(bottom)((acc, x1) => xs2.foldLeft(acc)((acc, x2) => join(acc, wrapped(abs.or(x1, x2)))))
+    }
+    def foldValues[B](s: PowerSet, f: PowerSet => Set[B]) = s match {
+      case Element(x) => abs.foldValues(x, (x) => f(Element(x)))
+      case Elements(xs) => xs.foldLeft(Set[B]())((acc: Set[B], x: X) => acc ++ abs.foldValues(x, (x) => f(Element(x))))
+    }
+    def join(p1: PowerSet, p2: PowerSet) = (p1, p2) match {
+      case (Element(x1), Element(x2)) => wrapped(abs.join(x1, x2))
+      case (Elements(xs), Element(x)) => join(p1, Elements(Set[X](x)))
+      case (Element(x), Elements(xs)) => join(Elements(Set[X](x)), p2)
+      case (Elements(xs1), Elements(xs2)) =>
+        /* every element in the other set has to be joined in this set */
+        Elements(xs2.foldLeft(xs1)((acc, x2) =>
+          if (acc.exists(x1 => abs.subsumes(x1, x2))) { acc } else { acc + x2 }))
+    }
+    def meet(p1: PowerSet, p2: PowerSet) = (p1, p2) match {
+      case (Element(x1), Element(x2)) => wrapped(abs.meet(x1, x2))
+      case (Elements(xs), Element(x)) => meet(p1, Elements(Set[X](x)))
+      case (Element(x), Elements(xs)) => meet(Elements(Set[X](x)), p2)
+      case (Elements(xs1), Elements(xs2)) =>
+        /* assumption: the elements contained in the set form a flat lattice,
+         * e.g., we will not need to compute the meet of {Int} with {1} */
+        Elements(xs1.intersect(xs2))
+    }
+    def subsumes(p1: PowerSet, p2: PowerSet) = (p1, p2) match {
+      case (Element(x1), Element(x2)) => abs.subsumes(x1, x2)
+      case (Elements(xs), Element(x2)) => xs.exists(x1 => abs.subsumes(x1, x2))
+      case (Element(x1), Elements(xs)) => xs.exists(x2 => abs.subsumes(x1, x2))
+      case (Elements(xs1), Elements(xs2)) =>
+        /* every element in xs2 should be subsumed by an element of xs1 */
+        xs2.forall(x2 => xs1.exists(x1 => abs.subsumes(x1, x2)))
+    }
+    def car[Addr : Address](p: PowerSet) = p match {
+      case Element(x) => abs.car[Addr](x)
+      case Elements(xs) => xs.flatMap(x => abs.car[Addr](x))
+    }
+    def cdr[Addr : Address](p: PowerSet) = p match {
+      case Element(x) => abs.cdr[Addr](x)
+      case Elements(xs) => xs.flatMap(x => abs.cdr[Addr](x))
+    }
+    def toString[Addr : Address](p: PowerSet, store: Store[Addr, PowerSet]) = p.toString /* TODO */
+    def getClosures[Exp : Expression, Addr : Address](p: PowerSet) = p match {
+      case Element(x) => abs.getClosures[Exp, Addr](x)
+      case Elements(xs) => xs.flatMap(x => abs.getClosures[Exp, Addr](x))
+    }
+    def getPrimitives[Addr : Address, Abs : AbstractValue](p: PowerSet) = p match {
+      case Element(x) => abs.getPrimitives[Addr, Abs](x)
+      case Elements(xs) => xs.flatMap(x => abs.getPrimitives[Addr, Abs](x))
+    }
+    def getTids[TID : ThreadIdentifier](p: PowerSet) = p match {
+      case Element(x) => abs.getTids[TID](x)
+      case Elements(xs) => xs.flatMap(x => abs.getTids[TID](x))
+    }
+    def bottom: PowerSet = Element(abs.bottom)
+    def error(p: PowerSet) = p match {
+      case Element(x) => Element(abs.error(x))
+      case Elements(xs) => xs.foldLeft(bottom)((acc, x) => join(acc, Element(abs.error(x))))
+    }
+    def inject(x: Int) = Element(abs.inject(x))
+    def inject(x: String) = Element(abs.inject(x))
+    def inject(x: Char) = Element(abs.inject(x))
+    def inject(x: Boolean) = Element(abs.inject(x))
+    def inject[Addr : Address, Abs : AbstractValue](x: Primitive[Addr, Abs]) = Element(abs.inject[Addr, Abs](x))
+    def inject[Exp : Expression, Addr : Address](x: (Exp, Environment[Addr])) = Element(abs.inject[Exp, Addr](x))
+    def injectTid[TID : ThreadIdentifier](t: TID) = Element(abs.injectTid[TID](t))
+    def injectSymbol(x: String) = Element(abs.inject(x))
+    def nil = Element(abs.nil)
+    def cons[Addr : Address](car: Addr, cdr: Addr) = Element(abs.cons(car, cdr))
+  }
+}
---
+++ src/main/scala/TypeLattice.scala
@@ -0,0 +1,199 @@
+import UnaryOperator._
+import BinaryOperator._
+
+/** Type lattice without top element. It distinguishes #t and #f, but also has a
+  * bool element (it is therefore *not* a flat lattice) */
+object TypeLattice {
+  trait Type {
+    def unaryOp(op: UnaryOperator): Type = op match {
+      case IsNull | IsCons | IsChar | IsSymbol | IsString | IsInteger | IsBoolean => False
+      case Not => False
+      case _ => Error
+    }
+    def binaryOp(op: BinaryOperator)(that: Type): Type = op match {
+      case Eq => throw CannotJoin[Type](True, False)
+      case _ => Error
+    }
+  }
+  object Error extends Type {
+    override def toString = "error"
+    override def unaryOp(op: UnaryOperator) = this
+    override def binaryOp(op: BinaryOperator)(that: Type) = this
+  }
+  object Int extends Type {
+    override def toString = "Int"
+    override def unaryOp(op: UnaryOperator) = op match {
+      case IsInteger => True
+      case Ceiling | Log | Random => Int
+      case _ => super.unaryOp(op)
+    }
+    override def binaryOp(op: BinaryOperator)(that: Type) = that match {
+      case Int => op match {
+        case Plus | Minus | Times | Div | Modulo => Int
+        case Lt | NumEq => throw CannotJoin[Type](True, False)
+        case _ => super.binaryOp(op)(that)
+      }
+      case _ => super.binaryOp(op)(that)
+    }
+  }
+  object String extends Type {
+    override def toString = "String"
+    override def unaryOp(op: UnaryOperator) = op match {
+      case IsString => True
+      case _ => super.unaryOp(op)
+    }
+  }
+  object Char extends Type {
+    override def toString = "Char"
+    override def unaryOp(op: UnaryOperator) = op match {
+      case IsChar => True
+      case _ => super.unaryOp(op)
+    }
+  }
+  object Symbol extends Type {
+    override def toString = "Symbol"
+    override def unaryOp(op: UnaryOperator) = op match {
+      case IsSymbol => True
+      case _ => super.unaryOp(op)
+    }
+  }
+  object True extends Type {
+    override def toString = "#t"
+    override def unaryOp(op: UnaryOperator) = op match {
+      case IsBoolean => True
+      case Not => True
+      case _ => super.unaryOp(op)
+    }
+    override def binaryOp(op: BinaryOperator)(that: Type) = op match {
+      case Eq => that match {
+        case True => True
+        case _ => super.binaryOp(op)(that)
+      }
+    }
+  }
+  object False extends Type {
+    override def toString = "#f"
+    override def unaryOp(op: UnaryOperator) = op match {
+      case IsBoolean => True
+      case Not => False
+      case _ => super.unaryOp(op)
+    }
+    override def binaryOp(op: BinaryOperator)(that: Type) = op match {
+      case Eq => that match {
+        case False => True
+        case _ => super.binaryOp(op)(that)
+      }
+    }
+  }
+  object Bottom extends Type {
+    override def toString = "⊥"
+    override def unaryOp(op: UnaryOperator) = this
+    override def binaryOp(op: BinaryOperator)(that: Type) = this
+  }
+  case class Prim[Addr : Address, Abs : AbstractValue](prim: Primitive[Addr, Abs]) extends Type {
+    override def toString = s"#<prim ${prim.name}>"
+  }
+  case class Clo[Exp : Expression, Addr : Address](clo: (Exp, Environment[Addr])) extends Type {
+    override def toString = "#<clo>"
+  }
+  case class Tid[TID : ThreadIdentifier](t: TID) extends Type {
+    override def toString = s"#<thread $t>"
+  }
+  object Nil extends Type {
+    override def toString = "()"
+    override def unaryOp(op: UnaryOperator) = op match {
+      case IsNull => True
+      case _ => super.unaryOp(op)
+    }
+    override def binaryOp(op: BinaryOperator)(that: Type) =
+      if (op == Eq && that == Nil) { True } else { super.binaryOp(op)(that) }
+  }
+  case class Cons[Addr : Address](car: Addr, cdr: Addr) extends Type {
+    override def toString = "#<cons>"
+    override def unaryOp(op: UnaryOperator) = op match {
+      case IsCons => True
+      case _ => super.unaryOp(op)
+    }
+  }
+
+  implicit object TypeAbstractValue extends AbstractValue[Type] {
+    def name = "Type"
+    def isTrue(x: Type) = x match {
+      case False => false
+      case Error => false
+      case Bottom => false
+    }
+    def isFalse(x: Type) = x match {
+      case False => true
+      case _ => false
+    }
+    def isError(x: Type) = x == Error
+    def unaryOp(op: UnaryOperator)(x: Type) = x.unaryOp(op)
+    def binaryOp(op: BinaryOperator)(x: Type, y: Type) = x.binaryOp(op)(y)
+    def foldValues[B](x: Type, f: Type => Set[B]) = f(x)
+    def join(x: Type, y: Type) = if (x == y) { x } else { throw CannotJoin[Type](x, y) }
+    def meet(x: Type, y: Type) = if (x == y) { x } else { bottom }
+    def subsumes(x: Type, y: Type) = x == y || (x == Bottom && (y == True || y == False))
+    def and(x: Type, y: => Type) = x match {
+      case False => False
+      case _ => y
+    }
+    def or(x: Type, y: => Type) = x match {
+      case False => y
+      case _ => x
+    }
+    def car[Addr : Address](x: Type) = x match {
+      case Cons(car: Addr, cdr: Addr) => Set(car)
+      case _ => Set()
+    }
+    def cdr[Addr : Address](x: Type) = x match {
+      case Cons(car: Addr, cdr: Addr) => Set(cdr)
+      case _ => Set()
+    }
+
+    private def toString[Addr : Address](x: Type, store: Store[Addr, Type], inside: Boolean, visited: Set[Type]): String = x match {
+      case Cons(car : Addr, cdr : Addr) =>
+        if (visited.contains(x)) {
+          "#loop"
+        } else {
+          val carstr =  toString(store.lookup(car), store, false, visited + x)
+          val cdrval = store.lookup(cdr)
+          val cdrstr =  toString(store.lookup(cdr), store, true, visited + x)
+          val content = cdrval match {
+            case Nil => s"$carstr"
+            case Cons(_, _) => s"$carstr $cdrstr"
+            case _ => s"$carstr . $cdrstr"
+          }
+          if (inside) { content } else { s"($content)" }
+        }
+      case _ => x.toString
+    }
+    def toString[Addr : Address](x: Type, store: Store[Addr, Type]) = toString(x, store, false, Set())
+
+    def getClosures[Exp : Expression, Addr : Address](x: Type) = x match {
+      case v: Clo[Exp, Addr] => Set(v.clo)
+      case _ => Set()
+    }
+    def getPrimitives[Addr : Address, Abs : AbstractValue](x: Type) = x match {
+      case Prim(prim: Primitive[Addr, Abs]) => Set(prim)
+      case _ => Set()
+    }
+    def getTids[TID : ThreadIdentifier](x: Type) = x match {
+      case Tid(t: TID) => Set(t)
+      case _ => Set()
+    }
+
+    def bottom = Bottom
+    def error(x: Type) = Error
+    def inject(x: Int) = Int
+    def inject(x: String) = String
+    def inject(x: Boolean) = if (x) { True } else { False }
+    def inject(x: Char) = Char
+    def inject[Addr : Address, Abs : AbstractValue](x: Primitive[Addr, Abs]) = Prim(x)
+    def inject[Exp : Expression, Addr : Address](x: (Exp, Environment[Addr])) = Clo[Exp, Addr](x)
+    def injectTid[TID : ThreadIdentifier](t: TID) = Tid(t)
+    def injectSymbol(x: String) = Symbol
+    def nil = Nil
+    def cons[Addr : Address](car: Addr, cdr: Addr) = Cons(car, cdr)
+  }
+}
