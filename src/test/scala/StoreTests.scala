b0d454288948f3be6b0c9c49d8def210d34bdb49
Quentin Stievenart <quentin.stievenart@gmail.com>
2016-02-18 13:17:35 +0100

    Add some store tests

---
+++ src/test/scala/StoreTests.scala
@@ -0,0 +1,40 @@
+import org.scalatest._
+import org.scalatest.prop._
+import org.scalacheck.Gen
+
+abstract class StorePropSpec[Addr : Address, Abs : AbstractValue]
+extends PropSpec with GeneratorDrivenPropertyChecks with Matchers {
+  val abs = implicitly[AbstractValue[Abs]]
+  val addr = implicitly[Address[Addr]]
+
+  val absint: Gen[Abs] = for { n: Int <- Gen.choose(-100, 100) } yield abs.inject(n)
+  val absstr: Gen[Abs] = for { s <- Gen.alphaStr } yield abs.inject(s)
+  val absbool: Gen[Abs] = Gen.oneOf(abs.inject(true), abs.inject(false))
+  val abschar: Gen[Abs] = for { c <- Gen.alphaNumChar } yield abs.inject(c)
+  val abssym: Gen[Abs] = for { s <- Gen.identifier } yield abs.injectSymbol(s)
+  /* TODO: cons, vectors */
+
+  val abstractValues: Gen[Abs] = Gen.oneOf(absint, absstr, absbool, abschar, abssym)
+
+  property("lookup after extend yields same object") { forAll(abstractValues) { v =>
+    val a = addr.primitive("foo")
+    val store = Store.empty[Addr, Abs].extend(a, v)
+    assert(v == store.lookup(a))
+  }}
+
+  property("lookupBot returns bot for unbound values") {
+    val a = addr.primitive("foo")
+    val store = Store.empty[Addr, Abs]
+    assert(abs.bottom == store.lookupBot(a))
+  }
+
+  property("store with abstract counting extended twice or more remains the same") { forAll(abstractValues) { v =>
+    val a = addr.primitive("foo")
+    val store = Store.empty[Addr, Abs](true).extend(a, v).extend(a, v)
+    assert(store == store.extend(a, v))
+  }}
+}
+
+class ConcreteStoreTest extends StorePropSpec[ClassicalAddress, ConcreteLattice.L]
+class TypeStoreTest extends StorePropSpec[ClassicalAddress, TypeLattice.L]
+class TypeSetStoreTest extends StorePropSpec[ClassicalAddress, TypeSetLattice.L]
