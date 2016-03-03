4529a1717ab234341889b372e71b4a8a1773ecd1
Quentin Stievenart <quentin.stievenart@gmail.com>
2016-03-03 15:41:04 +0100

    Add map to dderiv

--- test/dderiv.scm
+++ test/dderiv.scm
@@ -1,3 +1,11 @@
+(define (map f l)
+  (if (null? l)
+      l
+      (if (pair? l)
+          (cons (f (car l)) (map f (cdr l)))
+          (error "Cannot map over a non-list"))))
+
+
 (define (lookup key table)
   (letrec ((loop (lambda (x)
                    (if (null? x)
@@ -32,6 +40,7 @@
         (if f
             (f a)
             (error "No derivation method available")))))
+
 (define (my+dderiv a)
   (cons '+
         (map dderiv (cdr a))))
