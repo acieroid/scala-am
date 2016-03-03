2c3fe6506f592d3e6c76255b79682796750b1007
Quentin Stievenart <quentin.stievenart@gmail.com>
2016-03-03 14:22:49 +0100

    Improve modulo

--- src/main/scala/SchemeOps.scala
+++ src/main/scala/SchemeOps.scala
@@ -1,12 +1,12 @@
 object SchemeOps {
   def modulo(n1: Int, n2: Int): Int =
-    if (n1 * n2 < 0) {
+    if (scala.math.signum(n1) * scala.math.signum(n2) < 0) {
       /* different sign, behaviour not the same between Scheme and Scala, adjust it */
-      (Math.abs(n2) - Math.abs(n1) % Math.abs(n2)) % Math.abs(n2) * (if (n2 < 0) -1 else 1)
+      (scala.math.abs(n2) - scala.math.abs(n1) % scala.math.abs(n2)) % scala.math.abs(n2) * (if (n2 < 0) -1 else 1)
     } else {
       /* same sign, same behaviour */
       n1 % n2
     }
-  def random(n: Int): Int = Math.abs(scala.util.Random.nextInt % n)
-  def random(n: Float): Float = Math.abs(scala.util.Random.nextFloat % n)
+  def random(n: Int): Int = scala.math.abs(scala.util.Random.nextInt % n)
+  def random(n: Float): Float = scala.math.abs(scala.util.Random.nextFloat % n)
 }
