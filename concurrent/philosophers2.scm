b753f5eebf3629c44b48f2157fca163cee3a6b20
Quentin Stievenart <quentin.stievenart@gmail.com>
2016-03-08 13:55:55 +0100

    Update philisophers benchmarks

--- concurrent/philosophers2.scm
+++ concurrent/philosophers2.scm
@@ -1,26 +1,17 @@
 ;; Dining philosophers problem
-(letrec ((make-initialized-vector (lambda (n f)
-                                    (letrec ((v (make-vector n (bottom)))
-                                             (loop (lambda (i)
-                                                     (if (= i n)
-                                                         v
-                                                         (begin
-                                                           (vector-set! v i (f i))
-                                                           (loop (+ i 1)))))))
-                                      (loop 0)))))
-  (letrec ((n 2)                          ; number of philosophers
-           (turns 5)                      ; number of turns to run
-           (forks (make-initialized-vector n (lambda (x) (new-lock))))
-           (pickup (lambda (left right)
-                     (acquire (vector-ref forks (min left right)))
-                     (acquire (vector-ref forks (max left right)))))
-           (putdown (lambda (left right)
-                      (release (vector-ref forks (min left right)))
-                      (release (vector-ref forks (max left right)))))
-           (philosopher (lambda (i)
-                          (letrec ((left i)
-                                   (right (modulo (- i 1) n))
-                                   (process (lambda (turn)
+(letrec ((n 2)                          ; number of philosophers
+         (turns 5)                      ; number of turns to run
+         (forks (vector (new-lock) (new-lock)))
+         (pickup (lambda (left right)
+                   (acquire (vector-ref forks (min left right)))
+                   (acquire (vector-ref forks (max left right)))))
+         (putdown (lambda (left right)
+                    (release (vector-ref forks (min left right)))
+                    (release (vector-ref forks (max left right)))))
+         (philosopher (lambda (i)
+                        (letrec ((left i)
+                                 (right (modulo (- i 1) n))
+                                 (process (lambda (turn)
                                             (if (> turn turns)
                                                 #t
                                                 (begin
@@ -28,8 +19,8 @@
                                                   (display i) (newline)
                                                   (putdown left right)
                                                   (process (+ turn 1)))))))
-                            (process 0))))
-           (t1 (spawn (philosopher 0)))
-           (t2 (spawn (philosopher 1))))
-    (join t1)
-    (join t2)))
+                          (process 0))))
+         (t1 (spawn (philosopher 0)))
+         (t2 (spawn (philosopher 1))))
+  (join t1)
+  (join t2))
--- concurrent/philosophers3.scm
+++ concurrent/philosophers3.scm
@@ -1,37 +1,28 @@
 ;; Dining philosophers problem
-(letrec ((make-initialized-vector (lambda (n f)
-                                    (letrec ((v (make-vector n (bottom)))
-                                             (loop (lambda (i)
-                                                     (if (= i n)
-                                                         v
-                                                         (begin
-                                                           (vector-set! v i (f i))
-                                                           (loop (+ i 1)))))))
-                                      (loop 0)))))
-  (letrec ((n 3) ; number of philosophers
-           (turns 5) ; number of turns to run
-           (forks (make-initialized-vector n (lambda (x) (new-lock))))
-           (pickup (lambda (left right)
-                     (acquire (vector-ref forks (min left right)))
-                     (acquire (vector-ref forks (max left right)))))
-           (putdown (lambda (left right)
-                      (release (vector-ref forks (min left right)))
-                      (release (vector-ref forks (max left right)))))
-           (philosopher (lambda (i)
-                          (letrec ((left i)
-                                   (right (modulo (- i 1) n))
-                                   (process (lambda (turn)
-                                              (if (> turn turns)
-                                                  #t
-                                                  (begin
-                                                    (pickup left right)
-                                                    (display i) (newline)
-                                                    (putdown left right)
+(letrec ((n 3) ; number of philosophers
+         (turns 5) ; number of turns to run
+         (forks (vector (new-lock) (new-lock) (new-lock)))
+         (pickup (lambda (left right)
+                   (acquire (vector-ref forks (min left right)))
+                   (acquire (vector-ref forks (max left right)))))
+         (putdown (lambda (left right)
+                    (release (vector-ref forks (min left right)))
+                    (release (vector-ref forks (max left right)))))
+         (philosopher (lambda (i)
+                        (letrec ((left i)
+                                 (right (modulo (- i 1) n))
+                                 (process (lambda (turn)
+                                            (if (> turn turns)
+                                                #t
+                                                (begin
+                                                  (pickup left right)
+                                                  (display i) (newline)
+                                                  (putdown left right)
                                                   (process (+ turn 1)))))))
-                            (process 0))))
-           (t1 (spawn (philosopher 0)))
-           (t2 (spawn (philosopher 1)))
-           (t3 (spawn (philosopher 2))))
-    (join t1)
-    (join t2)
-    (join t3)))
+                          (process 0))))
+         (t1 (spawn (philosopher 0)))
+         (t2 (spawn (philosopher 1)))
+         (t3 (spawn (philosopher 2))))
+  (join t1)
+  (join t2)
+  (join t3))
