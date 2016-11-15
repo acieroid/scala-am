5dbdc2bd770d4877eca49b0dfe90e16f49850c8d
Quentin Stievenart <quentin.stievenart@gmail.com>
2016-11-15 14:57:34 +0100

    Add position information when sending messages

--- src/main/scala/semantics/ascheme/ASchemeSemantics.scala
+++ src/main/scala/semantics/ascheme/ASchemeSemantics.scala
@@ -1,12 +1,43 @@
 import scalaz.Scalaz._
 import scalaz._
 
-class ASchemeSemantics[Abs : IsASchemeLattice, Addr : Address, Time : Timestamp, PID : ThreadIdentifier](primitives: Primitives[Addr, Abs])
+abstract class ActorVisitor[Abs : JoinLattice] {
+  def send(pos: Position, target: Abs, message: String, args: List[Abs]): Unit
+}
+class EmptyActorVisitor[Abs : JoinLattice] extends ActorVisitor[Abs] {
+  def send(pos: Position, target: Abs, message: String, args: List[Abs]) = ()
+}
+class RecordActorVisitor[Abs : JoinLattice] extends ActorVisitor[Abs] {
+  var targetVals = Map.empty[Position, Abs].withDefaultValue(JoinLattice[Abs].bottom)
+  var argsVals = Map.empty[Position, List[Abs]]
+  def send(pos: Position, target: Abs, message: String, args: List[Abs]): Unit = {
+    println(s"send $pos")
+    targetVals += (pos -> (JoinLattice[Abs].join(targetVals(pos), target)))
+    argsVals += (pos -> (argsVals.get(pos) match {
+      case Some(vs) if vs.length == args.length => vs.zip(args).map({ case (x, y) => JoinLattice[Abs].join(x, y) })
+      case Some(vs) => throw new Error("should not happen")
+      case None => args
+    }))
+  }
+  def print = {
+    targetVals.foreach({ case (k, v) =>
+      println(s"$k: $v")
+    })
+    println("----------")
+    argsVals.foreach({ case (k, v) =>
+      println(s"$k: $v")
+    })
+  }
+}
+
+class ASchemeSemantics[Abs : IsASchemeLattice, Addr : Address, Time : Timestamp, PID : ThreadIdentifier]
+  (primitives: Primitives[Addr, Abs],
+    visitor: ActorVisitor[Abs])
     extends SchemeSemantics[Abs, Addr, Time](primitives) {
   object ActorAction extends ActorActionHelpers[SchemeExp, Abs, Addr, Time, PID]
 
-  case class FrameSendTarget(message: Identifier, args: List[SchemeExp], env: Env) extends SchemeFrame
-  case class FrameSend(message: Identifier, target: Abs, argsv: List[Abs], args: List[SchemeExp], env: Env) extends SchemeFrame
+  case class FrameSendTarget(pos: Position, message: Identifier, args: List[SchemeExp], env: Env) extends SchemeFrame
+  case class FrameSend(pos: Position, message: Identifier, target: Abs, argsv: List[Abs], args: List[SchemeExp], env: Env) extends SchemeFrame
   case class FrameCreate(argsv: List[Abs], args: List[SchemeExp], exp: SchemeExp, env: Env) extends SchemeFrame
   case class FrameBecome(argsv: List[Abs], args: List[SchemeExp], env: Env) extends SchemeFrame
 
@@ -17,7 +48,7 @@ class ASchemeSemantics[Abs : IsASchemeLattice, Addr : Address, Time : Timestamp,
 
   override def stepEval(e: SchemeExp, env: Env, store: Sto, t: Time) = optimizeAtomic(e match {
     case a: SchemeActor => Action.value(IsASchemeLattice[Abs].injectActor[SchemeExp, Addr](a, env), store)
-    case SchemeSend(target, message, args, _) => Action.push(FrameSendTarget(message, args, env), target, env, store)
+    case SchemeSend(target, message, args, pos) => Action.push(FrameSendTarget(pos, message, args, env), target, env, store)
     case SchemeCreate(beh, args, _) => Action.push(FrameCreate(List(), args, beh, env), beh, env, store)
     case SchemeBecome(beh, args, _) => Action.push(FrameBecome(List(), args, env), beh, env, store)
     case SchemeTerminate(_) => ActorAction.terminate
@@ -43,7 +74,8 @@ class ASchemeSemantics[Abs : IsASchemeLattice, Addr : Address, Time : Timestamp,
       }
   }
 
-  private def send(target: Abs, message: String, args: List[Abs]): Set[Action[SchemeExp, Abs, Addr]] = {
+  private def send(pos: Position, target: Abs, message: String, args: List[Abs]): Set[Action[SchemeExp, Abs, Addr]] = {
+    visitor.send(pos, target, message, args)
     val pids = IsASchemeLattice[Abs].getPids(target)
     if (pids.isEmpty) {
       Action.error(TypeError("send", "first operand", "pid value", s"non-pid value ($target)"))
@@ -53,15 +85,15 @@ class ASchemeSemantics[Abs : IsASchemeLattice, Addr : Address, Time : Timestamp,
   }
 
   override def stepKont(v: Abs, frame: Frame, store: Sto, t: Time) = optimizeAtomic(frame match {
-    case FrameSendTarget(message, List(), env) =>
-      send(v, message.name, List())
-    case FrameSendTarget(message, first :: rest, env) =>
-      Action.push(FrameSend(message, v, List(), rest, env), first, env, store)
-    case FrameSend(message, target, revargsv, List(), env) =>
+    case FrameSendTarget(pos, message, List(), env) =>
+      send(pos, v, message.name, List())
+    case FrameSendTarget(pos, message, first :: rest, env) =>
+      Action.push(FrameSend(pos, message, v, List(), rest, env), first, env, store)
+    case FrameSend(pos, message, target, revargsv, List(), env) =>
       val argsv = (v :: revargsv).reverse
-      send(target, message.name, (v :: revargsv).reverse)
-    case FrameSend(message, target, argsv, first :: rest, env) =>
-      Action.push(FrameSend(message, target, v :: argsv, rest, env), first, env, store)
+      send(pos, target, message.name, (v :: revargsv).reverse)
+    case FrameSend(pos, message, target, argsv, first :: rest, env) =>
+      Action.push(FrameSend(pos, message, target, v :: argsv, rest, env), first, env, store)
     case FrameCreate(revargsv, List(), exp, env) =>
       val act :: argsv = (v :: revargsv).reverse
       val actors = IsASchemeLattice[Abs].getActors[SchemeExp, Addr](act)
