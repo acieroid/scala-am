88fa1b8c973609a9fedafb1766741c71ab6a9e65
Quentin Stievenart <quentin.stievenart@gmail.com>
2016-11-21 16:23:48 +0100

    Add stack actor benchmark

---
+++ actors/stack.scm
@@ -0,0 +1,20 @@
+;; From Agha 1986, p. 52
+(letrec ((stack-node (actor "stack-node" (content link)
+                            (pop (customer)
+                                 (if content
+                                     (begin
+                                       (send customer message content)
+                                       (link))
+                                     (begin
+                                       (error "popping an empty stack")
+                                       (terminate))))
+                            (push (v)
+                                  (become stack-node v (lambda ()  (create stack-node content link))))))
+         (display-actor (actor "display" ()
+                               (message (v) (display v) (become display-actor))))
+         (disp (create display-actor))
+         (act (create stack-node #f #f)))
+  (send act push 1)
+  (send act push 2)
+  (send act push 3)
+  (send act pop disp))
