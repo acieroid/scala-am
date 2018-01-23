09db1deea520c15be9ac177b90dd9dd6f164cc22
Quentin Stievenart <quentin.stievenart@gmail.com>
2018-01-23 10:03:11 +0100

    Add scalability experiments for threads, macrostepping

--- src/main/scala/Experiments.scala
+++ src/main/scala/Experiments.scala
@@ -67,34 +67,34 @@ object ThreadExperiments {
 
 object ThreadExperimentsMacrostepping {
   val benchFiles: List[(String, String)] = List(
-//    ("ABP", "threads/suite/abp.scm"),
-//    ("COUNT", "threads/suite/count.scm"),
-//    ("DEKKER", "threads/suite/dekker.scm"),
-//    ("FACT", "threads/suite/fact.scm"),
-//    ("MATMUL", "threads/suite/matmul.scm"),
-//    ("MCARLO", "threads/suite/mcarlo.scm"),
-//    ("MSORT", "threads/suite/msort.scm"),
-//    ("PC", "threads/suite/pc.scm"),
-//    ("PHIL", "threads/suite/phil.scm"),
-//    ("PHILD", "threads/suite/phild.scm"),
-//    ("PP", "threads/suite/pp.scm"),
-//    ("RINGBUF", "threads/suite/ringbuf.scm"),
-//    ("RNG", "threads/suite/rng.scm"),
-//    ("SUDOKU", "threads/suite/sudoku.scm"),
-//    ("TRAPR", "threads/suite/trapr.scm"),
-//    ("ATOMS", "threads/suite/atoms.scm"),
-//    ("STM", "threads/suite/stm.scm"),
-//    ("NBODY", "threads/suite/nbody.scm"),
-//    ("SIEVE", "threads/suite/sieve.scm"),
-//    ("CRYPT", "threads/suite/crypt.scm"),
-//    ("MCEVAL", "threads/suite/mceval.scm"),
-//    ("QSORT", "threads/suite/qsort.scm"),
-//    ("TSP", "threads/suite/tsp.scm"),
-//    ("BCHAIN", "threads/suite/bchain.scm"),
-    ("LIFE", "threads/suite/life.scm")
-//    ("PPS", "threads/suite/pps.scm"),
-//    ("MINIMAX", "threads/suite/minimax.scm"),
-//    ("ACTORS", "threads/suite/actors.scm")
+    ("ABP", "threads/suite/abp.scm"),
+    ("COUNT", "threads/suite/count.scm"),
+    ("DEKKER", "threads/suite/dekker.scm"),
+    ("FACT", "threads/suite/fact.scm"),
+    ("MATMUL", "threads/suite/matmul.scm"),
+    ("MCARLO", "threads/suite/mcarlo.scm"),
+    ("MSORT", "threads/suite/msort.scm"),
+    ("PC", "threads/suite/pc.scm"),
+    ("PHIL", "threads/suite/phil.scm"),
+    ("PHILD", "threads/suite/phild.scm"),
+    ("PP", "threads/suite/pp.scm"),
+    ("RINGBUF", "threads/suite/ringbuf.scm"),
+    ("RNG", "threads/suite/rng.scm"),
+    ("SUDOKU", "threads/suite/sudoku.scm"),
+    ("TRAPR", "threads/suite/trapr.scm"),
+    ("ATOMS", "threads/suite/atoms.scm"),
+    ("STM", "threads/suite/stm.scm"),
+    ("NBODY", "threads/suite/nbody.scm"),
+    ("SIEVE", "threads/suite/sieve.scm"),
+    ("CRYPT", "threads/suite/crypt.scm"),
+    ("MCEVAL", "threads/suite/mceval.scm"),
+    ("QSORT", "threads/suite/qsort.scm"),
+    ("TSP", "threads/suite/tsp.scm"),
+    ("BCHAIN", "threads/suite/bchain.scm"),
+    ("LIFE", "threads/suite/life.scm"),
+    ("PPS", "threads/suite/pps.scm"),
+    ("MINIMAX", "threads/suite/minimax.scm"),
+    ("ACTORS", "threads/suite/actors.scm")
   )
   def main(args: Array[String]): Unit = {
     val lat = new MakeCSchemeLattice[ScalaAM.typeLattice.L]
@@ -198,7 +198,6 @@ object ThreadExperimentsNaive {
     }
   }
 }
-
 
 object ThreadExperimentsScalabilityProcesses {
   import java.io._
@@ -368,6 +367,211 @@ object ThreadExperimentsScalabilityConflicts {
     val time: ActorTimestampWrapper = KMessageTagSensitivity(0)
     implicit val isActorTimestamp = time.isActorTimestamp
     val machine = new ConcurrentModular[SchemeExp, lat.L, ClassicalAddress.A, time.T, ContextSensitiveTID](false)
+    val sem = new CSchemeSemantics[lat.L, ClassicalAddress.A, time.T, ContextSensitiveTID](new CSchemePrimitives[ClassicalAddress.A, lat.L])
+
+    val run = ScalaAM.run[SchemeExp, lat.L, ClassicalAddress.A, time.T](machine, sem) _
+
+    val writer = new PrintWriter(new File("scalability.dat"))
+    val N = 20
+    val warmup = 10
+    for (i <- 1 to MAX) {
+      val sname = (i.toString).padTo(10, " ").mkString
+      val file = s"/tmp/p${i}.scm"
+      gen(i, file)
+
+      Util.fileContent(file) match {
+        case Some(program) =>
+          val times: List[Double] = (1 to N+warmup).map(i => {
+            val time = scala.Console.withOut(new java.io.OutputStream { override def write(b: Int) {} }) {
+              run(program, false, timeout).time
+            }
+            if (i <= warmup) {} else {
+              val itime = (time * 1000).toInt
+              println(s"$sname | $itime")
+              writer.write(s"$sname $itime\n")
+            }
+            time
+          }).toList
+          val mean = (times.drop(warmup).sum / N * 1000).toInt
+          println(s"MEAN: $mean")
+        case None => ()
+      }
+      writer.flush
+      // rm(file)
+      System.gc()
+    }
+    writer.close
+  }
+}
+
+object ThreadExperimentsScalabilityProcessesMacrostepping {
+  import java.io._
+  import java.nio.file.Files
+  val MAX = 150
+  def rm(file: String) = {
+    val f = new File(file)
+    f.delete()
+  }
+  def gen(processes: Int, file: String) = {
+    val writer = new PrintWriter(new File(file))
+    val name = "\"foo\""
+    writer.write("(letrec ((f (lambda (n) n))\n")
+    for (i <- 0 until MAX) {
+      if (i <= processes) {
+        writer.write(s"       (t$i (t/spawn (f $i)))\n")
+      } else {
+        writer.write(s"       (t$i $i)\n")
+      }
+    }
+    writer.write(")\n")
+    writer.write("(f 1))\n")
+    writer.close
+  }
+
+  def main(args: Array[String]): Unit = {
+    val lat = new MakeCSchemeLattice[ScalaAM.typeLattice.L]
+    val timeout: Option[Long] = Some(120 * 1e9.toLong)
+    implicit val isCSchemeLattice = lat.isCSchemeLattice
+    val time: ActorTimestampWrapper = KMessageTagSensitivity(0)
+    implicit val isActorTimestamp = time.isActorTimestamp
+    val machine = new ConcurrentAAMGS[SchemeExp, lat.L, ClassicalAddress.A, time.T, ContextSensitiveTID](Macrostepping, false)
+    val sem = new CSchemeSemantics[lat.L, ClassicalAddress.A, time.T, ContextSensitiveTID](new CSchemePrimitives[ClassicalAddress.A, lat.L])
+
+    val run = ScalaAM.run[SchemeExp, lat.L, ClassicalAddress.A, time.T](machine, sem) _
+
+    val writer = new PrintWriter(new File("scalability.dat"))
+    val N = 20
+    val warmup = 10
+    for (i <- 1 to MAX) {
+      val sname = (i.toString).padTo(10, " ").mkString
+      val file = s"/tmp/p${i}.scm"
+      gen(i, file)
+
+      Util.fileContent(file) match {
+        case Some(program) =>
+          (1 to N+warmup).foreach(i => {
+            val time = scala.Console.withOut(new java.io.OutputStream { override def write(b: Int) {} }) {
+              run(program, false, timeout).time
+            }
+            if (i <= warmup) {} else {
+              val itime = (time * 1000).toInt
+              println(s"$sname | $itime")
+              writer.write(s"$sname $itime\n")
+            }
+          })
+        case None => ()
+      }
+      writer.flush
+      // rm(file)
+      System.gc()
+    }
+    writer.close
+  }
+}
+
+object ThreadExperimentsScalabilityJoinsMacrostepping {
+  import java.io._
+  import java.nio.file.Files
+  val MAX = 150
+  def rm(file: String) = {
+    val f = new File(file)
+    f.delete()
+  }
+  def gen(joins: Int, file: String) = {
+    val writer = new PrintWriter(new File(file))
+    val name = "\"foo\""
+    writer.write("(letrec ((f (lambda (n) f))\n")
+    for (i <- 0 until MAX+1) {
+      if (i < joins) {
+        writer.write(s"(t$i (t/spawn (f (t/join t${i+1}))))")
+      } else {
+        writer.write(s"(t$i (t/spawn (f (+ t${i+1}))))")
+      }
+    }
+    writer.write(")\n")
+    writer.write("(t/join t0)")
+    writer.write(")\n")
+    writer.close
+  }
+
+  def main(args: Array[String]): Unit = {
+    val lat = new MakeCSchemeLattice[ScalaAM.typeLattice.L]
+    val timeout: Option[Long] = Some(120 * 1e9.toLong)
+    implicit val isCSchemeLattice = lat.isCSchemeLattice
+    val time: ActorTimestampWrapper = KMessageTagSensitivity(0)
+    implicit val isActorTimestamp = time.isActorTimestamp
+    val machine = new ConcurrentAAMGS[SchemeExp, lat.L, ClassicalAddress.A, time.T, ContextSensitiveTID](Macrostepping, false)
+    val sem = new CSchemeSemantics[lat.L, ClassicalAddress.A, time.T, ContextSensitiveTID](new CSchemePrimitives[ClassicalAddress.A, lat.L])
+
+    val run = ScalaAM.run[SchemeExp, lat.L, ClassicalAddress.A, time.T](machine, sem) _
+
+    val writer = new PrintWriter(new File("scalability.dat"))
+    val N = 20
+    val warmup = 10
+    for (i <- 1 to MAX) {
+      val sname = (i.toString).padTo(10, " ").mkString
+      val file = s"/tmp/p${i}.scm"
+      gen(i, file)
+
+      Util.fileContent(file) match {
+        case Some(program) =>
+          val times: List[Double] = (1 to N+warmup).map(i => {
+            val time = scala.Console.withOut(new java.io.OutputStream { override def write(b: Int) {} }) {
+              run(program, false, timeout).time
+            }
+            if (i <= warmup) {} else {
+              val itime = (time * 1000).toInt
+            //println(s"$sname | $itime")
+            writer.write(s"$sname $itime\n")
+          }
+            time
+          }).toList
+          val mean = (times.drop(warmup).sum / N * 1000).toInt
+          println(s"MEAN: $mean")
+        case None => ()
+      }
+      writer.flush
+      // rm(file)
+      System.gc()
+    }
+    writer.close
+  }
+}
+
+object ThreadExperimentsScalabilityConflictsMacrostepping {
+  import java.io._
+  import java.nio.file.Files
+  val MAX = 150
+  def rm(file: String) = {
+    val f = new File(file)
+    f.delete()
+  }
+  def gen(joins: Int, file: String) = {
+    val writer = new PrintWriter(new File(file))
+    val name = "\"foo\""
+    writer.write("(letrec ((x (t/ref 0))\n")
+    for (i <- 0 until MAX) {
+      if (i < joins) {
+        writer.write(s"       (t$i (t/spawn (begin (t/ref-set x (+ (t/deref x) 1)))))\n")
+      } else {
+        writer.write(s"       (t$i (t/spawn $i))\n")
+      }
+    }
+    writer.write(")\n")
+    for (i <- 0 until MAX) {
+      writer.write("(t/join t$i)")
+    }
+    writer.write(")\n")
+    writer.close
+  }
+
+  def main(args: Array[String]): Unit = {
+    val lat = new MakeCSchemeLattice[ScalaAM.typeLattice.L]
+    val timeout: Option[Long] = Some(120 * 1e9.toLong)
+    implicit val isCSchemeLattice = lat.isCSchemeLattice
+    val time: ActorTimestampWrapper = KMessageTagSensitivity(0)
+    implicit val isActorTimestamp = time.isActorTimestamp
+    val machine = new ConcurrentAAMGS[SchemeExp, lat.L, ClassicalAddress.A, time.T, ContextSensitiveTID](Macrostepping, false)
     val sem = new CSchemeSemantics[lat.L, ClassicalAddress.A, time.T, ContextSensitiveTID](new CSchemePrimitives[ClassicalAddress.A, lat.L])
 
     val run = ScalaAM.run[SchemeExp, lat.L, ClassicalAddress.A, time.T](machine, sem) _
