a2895860f9db886b1412f3f8e86be5affaf8064d
Quentin Stiévenart <quentin.stievenart@gmail.com>
2019-02-05 16:15:20 +0100

    [benchmarks] fix browse

--- test/gabriel/browse.scm
+++ test/gabriel/browse.scm
@@ -1,3 +1,8 @@
+(define (append l m)
+  (if (null? l)
+      m
+      (cons (car l) (append (cdr l) m))))
+
 ;;; BROWSE -- Benchmark to create and browse through
 ;;; an AI-like data base of units.
 
