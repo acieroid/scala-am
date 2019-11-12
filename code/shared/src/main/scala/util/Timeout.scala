c5d258e50e8ff1db1656a12a4d2973f7a326283a
Jens Van der Plas <jevdplas@vub.be>
2019-11-12 16:18:16 +0100

    Fix deprecation deprecation warnings

--- code/shared/src/main/scala/util/Timeout.scala
+++ code/shared/src/main/scala/util/Timeout.scala
@@ -8,12 +8,12 @@ object Timeout {
     def time: Double     = (System.nanoTime - startingTime) / Math.pow(10, 9)
   }
 
-  @deprecated def start(timeout: Option[Long]): T = T(System.nanoTime, timeout)
+  // def start(timeout: Option[Long]): T = T(System.nanoTime, timeout)
   def start(timeout: Duration): T = T(System.nanoTime, if (timeout.isFinite) Some(timeout.toNanos) else None)
 
   val none = T(System.nanoTime, None)
 
-  @deprecated def minutes(n: Int): T = start(Some(n.toLong * 60 * 1000 * 1000 * 1000))
-  @deprecated def seconds(n: Int): T = start(Some(n.toLong * 1000 * 1000 * 1000))
-  @deprecated def duration(d: Duration): T = start(Some(d.toNanos))
+  // def minutes(n: Int): T = start(Some(n.toLong * 60 * 1000 * 1000 * 1000))
+  // def seconds(n: Int): T = start(Some(n.toLong * 1000 * 1000 * 1000))
+  // def duration(d: Duration): T = start(Some(d.toNanos))
 }
