30c89a4d46ad4b5c030c89e6ef56399d1eeedac9
Noah Van Es <nvanes@soft34.vub.ac.be>
2019-11-14 14:09:55 +0100

    (re)-added Scheme Parser tests

---
+++ code/shared/src/test/scala/language/scheme/ParserTests.scala
@@ -0,0 +1,23 @@
+package scalaam.test.parser
+
+import scalaam.test._
+
+import scalaam.language.scheme._
+
+trait SchemeParserTests extends SchemeBenchmarkTests {
+  def onBenchmark(benchmark: Benchmark) =
+    property(s"SchemeParser can correctly parse $benchmark") {
+      val content = loadFileText(benchmark)
+      val parsed = SchemeParser.parse(content)
+      // Check that the parsing was succesful
+      assert(parsed.toString.nonEmpty)
+      // Check that printing and parsing the result again gives the same result
+      val printed = parsed.toString
+      val reparsed = SchemeParser.parse(printed)
+      assert(parsed.toString== reparsed.toString,
+        "Printing and parsing again gives a result different from the original parse")
+    }
+}
+
+class SimpleSchemeParserTests extends SchemeParserTests
+                                with SimpleBenchmarks
