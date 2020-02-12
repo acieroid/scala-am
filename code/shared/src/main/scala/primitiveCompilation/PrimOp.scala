1dcfe7165d9d0b5fa34d4ca253e772ed4132ade1
Jens Van der Plas <jevdplas@vub.be>
2020-02-12 12:44:23 +0100

    Add some extra primops

--- code/shared/src/main/scala/primitiveCompilation/PrimOp.scala
+++ code/shared/src/main/scala/primitiveCompilation/PrimOp.scala
@@ -9,20 +9,36 @@ trait PrimOp {
 
 case object NumEq   extends PrimOp { val arity = -1; val name = "="; override def toString: String = "NumEq" }
 case object NumPlus extends PrimOp { val arity = -1; val name = "+"; override def toString: String = "Plus" }
+case object NumMult extends PrimOp { val arity = -1; val name = "*"; override def toString: String = "Times" }
+case object NumMin  extends PrimOp { val arity = -1; val name = "-"; override def toString: String = "Minus" }
+case object NumDiv  extends PrimOp { val arity = -1; val name = "/"; override def toString: String = "Div" }
+case object NumCos  extends PrimOp { val arity = 1; val name = "cos"; override def toString: String = "Cos" }
+case object NumSin  extends PrimOp { val arity = 1; val name = "sin"; override def toString: String = "Sin" }
+case object NumTan  extends PrimOp { val arity = 1; val name = "tan"; override def toString: String = "Tan" }
+case object NumAcos extends PrimOp { val arity = 1; val name = "acos"; override def toString: String = "ACos" }
+case object NumAsin extends PrimOp { val arity = 1; val name = "asin"; override def toString: String = "ASin" }
+case object NumAtan extends PrimOp { val arity = 1; val name = "atan"; override def toString: String = "ATan" }
 
+case object Boolp   extends PrimOp { val arity = 1; val name = "boolean?"; override def toString: String = "Booleanp" }
 case object And     extends PrimOp { val arity = 2; val name = "and"; override def toString: String = "And"}
 case object Or      extends PrimOp { val arity = 2; val name = "or"; override def toString: String = "Or"}
 
+case object Charp   extends PrimOp { val arity = 1; val name = "char?"; override def toString: String = "Charp"}
+
 case object Null    extends PrimOp { val arity = 1; val name = "null?"; override def toString: String = "Nullp"}
+case object Lst     extends PrimOp { val arity = -1; val name = "list"; override def toString: String = "List"}
 case object Cons    extends PrimOp { val arity = 2; val name = "cons"; override def toString: String = "Cons"}
 case object Car     extends PrimOp { val arity = 1; val name = "car"; override def toString: String = "Car"}
 case object Cdr     extends PrimOp { val arity = 1; val name = "cdr"; override def toString: String = "Cdr"}
+
 case object Vector  extends PrimOp { val arity = -1; val name = "vector"; override def toString: String = "Vector"} // TODO Fix arity
 case object VectRef extends PrimOp { val arity = 2; val name = "vector-ref"; override def toString: String = "VectorRef"}
 case object VectSet extends PrimOp { val arity = 3; val name = "vector-set!"; override def toString: String = "VectorSet"}
 
+case object Eq      extends PrimOp { val arity = 2; val name = "eq?"; override def toString: String = "Eq"}
+
 object PrimitiveOperations {
-  val    ops: List[PrimOp] = List(NumEq, NumPlus, And, Or, Null, Cons, Car, Cdr, Vector, VectRef, VectSet)
+  val    ops: List[PrimOp] = List(NumEq, NumPlus, NumMult, NumMin, NumDiv, NumCos, NumSin, NumTan, NumAcos, NumAsin, NumAtan, Boolp, And, Or, Charp, Null, Lst, Cons, Car, Cdr, Vector, VectRef, VectSet, Eq)
   val opNams: List[String] = ops.map(_.name)
   val storeOps: List[PrimOp] = List(Cons, Car, Cdr, Vector, VectRef, VectSet)
   val stoNams: List[String] = storeOps.map(_.name)
