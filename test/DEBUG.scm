acc420889e9bc35d98b61cd9825006a93615c32e
Jens Van der Plas <jevdplas@vub.be>
2020-03-26 14:43:23 +0100

    Implementing consistency check.

--- test/DEBUG.scm
+++ test/DEBUG.scm
@@ -1,22 +1,13 @@
-(define (put key1 key2 val) #t)
-
 (define (tree-copy x)
   (if (not (pair? x))
     x
     (cons (tree-copy (car x))
       (tree-copy (cdr x)))))
-
-(define (init n m npats ipats)
-  (let ((ipats (tree-copy ipats))) ; Removing this let makes things deterministic...
+(define (init n m ipats)
+  (let ((ipats (tree-copy ipats)))
     (do ((n n (- n 1))
-          (i m (cond ((zero? i) m)
-                 (else (- i 1))))
-          (name 's 's)
+          (i m (if (zero? i) m (- i 1)))
           (a '()))
       ((= n 0) a)
-      (set! a (cons name a))
-      (do ((j (- m i) (- j 1)))
-        ((zero? j))
-        (put name 's #f)))))
-
-(init 1 1 1 '((a a b b b b a a (b b)) (a (b a) b a)))
+      (set! a (cons 's a)))))
+(init 1 1 '(( a (b b)) (a (b a) b a)))
