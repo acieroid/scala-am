6507ffa55e8a3b0adb5a4c6beedc37b551b7c92c
Jens Van der Plas <jevdplas@vub.be>
2020-03-26 10:25:28 +0100

    Reduce DEBUG.scm

--- test/DEBUG.scm
+++ test/DEBUG.scm
@@ -1,9 +1,22 @@
-(define organigram
-    '(directeur
-       (hoofd-inkoop)))
-(define (collegas p oversten organigram)
-  (if (eq? p (car organigram))
-    (append oversten (append '(1) '(2)))
-    (collegas p
-      (car organigram) (cadr organigram))))
-(collegas 'hoofd-inkoop '() organigram)
+(define (put key1 key2 val) #t)
+
+(define (tree-copy x)
+  (if (not (pair? x))
+    x
+    (cons (tree-copy (car x))
+      (tree-copy (cdr x)))))
+
+(define (init n m npats ipats)
+  (let ((ipats (tree-copy ipats))) ; Removing this let makes things deterministic...
+    (do ((n n (- n 1))
+          (i m (cond ((zero? i) m)
+                 (else (- i 1))))
+          (name 's 's)
+          (a '()))
+      ((= n 0) a)
+      (set! a (cons name a))
+      (do ((j (- m i) (- j 1)))
+        ((zero? j))
+        (put name 's #f)))))
+
+(init 1 1 1 '((a a b b b b a a (b b)) (a (b a) b a)))
