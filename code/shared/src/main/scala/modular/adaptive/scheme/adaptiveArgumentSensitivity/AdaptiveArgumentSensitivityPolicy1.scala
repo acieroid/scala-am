4b727aa109f270ac2af947aaf82eb0e1c742b3a5
Noah Van Es <noahvanes@me.com>
2020-04-08 20:47:30 +0200

    policy #1 redone (using absolute threshold :-())

--- code/shared/src/main/scala/modular/adaptive/scheme/adaptiveArgumentSensitivity/AdaptiveArgumentSensitivityPolicy1.scala
+++ code/shared/src/main/scala/modular/adaptive/scheme/adaptiveArgumentSensitivity/AdaptiveArgumentSensitivityPolicy1.scala
@@ -38,3 +38,30 @@ trait AdaptiveArgumentSensitivityPolicy1 extends AdaptiveArgumentSensitivity
     closureCmps = updateMap(updateClosure(update),updateSet(update))(closureCmps)
   }
 }
+
+trait AdaptiveArgumentSensitivityPolicyAlt1 extends AdaptiveArgumentSensitivityAlt {
+  // parameterized by a simple limit
+  // every closure can only have at most "limit" components
+  val limit: Int
+  // track the number of components per closure
+  private var closureCmps = Map[lattice.Closure, Set[Component]]()
+  def adaptOnNewComponent(cmp: Component, call: Call): Boolean = {
+    val Call(clo, _, _) = call
+    // update the set of components per closure
+    val updatedCmps = closureCmps.get(clo).getOrElse(Set()) + cmp
+    closureCmps += (clo -> updatedCmps)
+    // if there are too many components => do something about it!
+    if (limit < updatedCmps.size) {
+      val callArgs = updatedCmps.map(view(_).asInstanceOf[Call].ctx.args)
+      joinArgs(clo, callArgs, limit)
+      return true
+    } else { // otherwise, not arguments need to be excluded
+      return false
+    }
+  }
+  // we need to update the `closureCmps` data structure when the analysis is adapted
+  override def updateAnalysisData(update: Component => Component) = {
+    super.updateAnalysisData(update)
+    closureCmps = updateMap(updateClosure(update),updateSet(update))(closureCmps)
+  }
+}
