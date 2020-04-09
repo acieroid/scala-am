59ac72a87fbe58ccd0ac814851aed2f71239a8d8
Quentin Stiévenart <quentin.stievenart@gmail.com>
2020-04-09 14:48:19 +0200

    Prelude primitives definitions in tests

--- code/shared/src/test/scala/SchemeBenchmarks.scala
+++ code/shared/src/test/scala/SchemeBenchmarks.scala
@@ -3,6 +3,7 @@ package scalaam.test
 import org.scalatest._
 
 import scalaam.language.scheme._
+import scalaam.core.{Expression, Identifier, Identity}
 
 trait SchemeBenchmarkTests extends PropSpec {
   // a benchmark is just a file name
@@ -16,8 +17,244 @@ trait SchemeBenchmarkTests extends PropSpec {
     f.close()
     txt
   }
+
+  val primDefs = Map(
+    // "*" is a primop
+    // "+" is a primop
+    // "-" is a primop
+    // "/" is a primop
+    "abs" -> "(define (abs x) (if (< x 0) (- 0 x) x))",
+    // "acos" is a primop
+    "append" -> """(define (append l1 l2)
+          (if (null? l1)
+              l2
+              (cons (car l1)
+                    (append (cdr l1) l2))))""",
+    // "asin" is a primop
+    "assoc" -> """(define (assoc k l)
+        (if (null? l)
+          #f
+         (if (equal? (caar l) k)
+           (car l)
+           (assoc k (cdr l)))))""",
+    "assq" -> """(define (assq k l)
+        (if (null? l)
+          #f
+         (if (eq? (caar l) k)
+           (car l)
+           (assq k (cdr l)))))""",
+    "assv" -> """(define (assv k l)
+        (if (null? l)
+          #f
+         (if (eqv? (caar l) k)
+           (car l)
+           (assq k (cdr l)))))""",
+    // "atan" is a primop
+    // "boolean?" is a primop
+    // "car" is a primop
+    // "cdr" is a primop
+    // "ceiling" is a primop
+    // "char->integer" is a primop
+    // "char?" is a primop
+    // "cons" is a primop? TODO maybe not exactly (could define in terms of vectors of size 2)
+    // "cos" is a primop
+    "display" -> "(define (display x) x)", // undefined behavior in R5RS
+    // "eq?" is a primop
+    // TODO: vectors in equal? (requires a loop, see SchemePrimitives.scala)
+    "equal?" -> """(define (equal? a b)
+          (or (eq? a b)
+            (and (null? a) (null? b))
+            (and (pair? a) (pair? b) (equal? (car a) (car b)) (equal? (cdr a) (cdr b)))))""",
+    "even?" -> "(define (even? x) (= 0 (modulo x 2)))",
+    // "exact->inexact" is a primop
+    // TODO: expt // TODO isn't this a primot (easier to handle real exponents).
+    // TODO: exp
+    // "floor" is a primop
+    "gcd" -> "(define (gcd a b) (if (= b 0) a (gcd b (modulo a b))))",
+    // "inexact->exact" is a primop
+    // "integer?" is a primop
+    "lcm" -> "(define (lcm m n) (/ (abs (* m n)) (gcd m n)))",
+    /*
+    (define (lcm m n)
+      (define (generate cur prim)
+        (let ((flag #f))
+          (for-each (lambda (p) (set! flag (or flag (= 0 (modulo cur p))))) prim)
+          (if (not flag) ; Found a prime.
+              (cons cur (delay (generate (+ cur 1) (cons cur prim))))
+              (generate (+ cur 1) prim))))
+      (define primes (generate 2 '()))
+      (define (next-prime primes)
+        (force (cdr primes)))
+      (define (factor n)
+        (define (f n div p)
+          (if (= n 1)
+              div
+              (if (= 0 (modulo n (car p)))
+                  (f (/ n (car p)) (cons (car p) div) p)
+                  (f n div (next-prime p)))))
+        (f n '() primes))
+      (let loop ((f1 (factor m))
+                 (f2 (factor n))
+                 (cur 1))
+        (cond ((or (null? f1)(null? f2)) cur)
+              ((= (car f1)(car f2))(loop (cdr f1)(cdr f2)(* cur (car f1))))
+              ((> (car f1)(car f2))(loop (cdr f1) f2 (* cur (car f1))))
+              (else (loop f1 (cdr f2) (* cur (car f2)))))))
+     */
+    "length" -> """(define (length l)
+          (if (null? l)
+              0
+              (+ 1 (length (cdr l)))))""",
+    // "list" is a primop of variadic arity
+    "list-ref" -> """(define (list-ref l index)
+            (if (= index 0)
+              (car l)
+              (list-ref (cdr l) (- index 1))))""",
+    "list-tail" -> """(define (list-tail x k)
+                     |    (if (zero? k)
+                     |        x
+                     |        (list-tail (cdr x) (- k 1))))""".stripMargin, // Based on definition in R5RS specification.
+    "list?" -> "(define (list? l) (or (and (pair? l) (list? (cdr l))) (null? l)))",
+    // "log" is a primop
+    "max" -> "(define (max a b) (if (< a b) b a))", // variadic
+    "member" -> """(define (member e l)
+          (if (null? l)
+            #f
+            (if (equal? (car l) e)
+              l
+              (member e (cdr l)))))""",
+    "memq" -> """(define (memq e l)
+          (if (null? l)
+            #f
+            (if (eq? (car l) e)
+              l
+              (memq e (cdr l)))))""",
+    "min" -> "(define (min a b) (if (< a b) a b))", // variadic
+    // "modulo" is a primop
+    "negative?" -> "(define (negative? x) (< x 0))",
+    "newline" -> "(define (newline) #f)", // undefined
+    "not" -> "(define (not x) (if x #f #t))",
+    // "null?" is a primop
+    // "number->string" is a primop
+    // "number?" is a primop
+    "odd?" -> "(define (odd? x) (= 1 (modulo x 2)))",
+    // "pairp?" is a primop, or is it? TODO
+    "positive?" -> "(define (positive? x) (> x 0))",
+    // "quotient" is a primop
+    // "real?" is a primop
+    // "remainder" is a primop
+    // "round" is a primop
+    // "set-car!" is a primop
+    // "set-cdr!" is a primop
+    // "sin" is a primop
+    // TODO: sqrt
+    // "string->symbol" is a primop
+    // "string-append" is a primop
+    // "string-length" is a primop
+    // "string-ref" is a primop
+    // "string-lt" is a primop
+    // "string?" is a primop
+    // "symbol->string" is a primop
+    // "tan" is a primop
+    // TODO: make-vector
+    // TODO: vector
+    // TODO: vector-length
+    // TODO: vector-ref
+    // TODO: vector-set
+    // TODO: vector?
+    "zero?" -> "(define (zero? x) (= x 0))",
+    // "<" is a primop
+    "<=" -> "(define (<= x y) (or (< x y) (= x y)))",
+    // "=" is a primop
+    ">" -> "(define (> x y) (not (<= x y)))",
+    ">=" -> "(define (>= x y) (or (> x y) (= x y)))",
+
+    "caar" -> "(define (caar x) (car (car x)))",
+    "cadr" -> "(define (cadr x) (car (cdr x)))",
+    "cdar" -> "(define (cdar x) (cdr (car x)))",
+    "cddr" -> "(define (cddr x) (cdr (cdr x)))",
+    "caaar" -> "(define (caaar x) (car (car (car x))))",
+    "caadr" -> "(define (caadr x) (car (car (cdr x))))",
+    "cadar" -> "(define (cadar x) (car (cdr (car x))))",
+    "caddr" -> "(define (caddr x) (car (cdr (cdr x))))",
+    "cdaar" -> "(define (cdaar x) (cdr (car (car x))))",
+    "cdadr" -> "(define (cdadr x) (cdr (car (cdr x))))",
+    "cddar" -> "(define (cddar x) (cdr (cdr (car x))))",
+    "cdddr" -> "(define (cdddr x) (cdr (cdr (cdr x))))",
+    "caaaar" -> "(define (caaaar x) (car (car (car (car x)))))",
+    "caaadr" -> "(define (caaadr x) (car (car (car (cdr x)))))",
+    "caadar" -> "(define (caadar x) (car (car (cdr (car x)))))",
+    "caaddr" -> "(define (caaddr x) (car (car (cdr (cdr x)))))",
+    "cadaar" -> "(define (cadaar x) (car (cdr (car (car x)))))",
+    "cadadr" -> "(define (cadadr x) (car (cdr (car (cdr x)))))",
+    "caddar" -> "(define (caddar x) (car (cdr (cdr (car x)))))",
+    "cadddr" -> "(define (cadddr x) (car (cdr (cdr (cdr x)))))",
+    "cdaaar" -> "(define (cdaaar x) (cdr (car (car (car x)))))",
+    "cdaadr" -> "(define (cdaadr x) (cdr (car (car (cdr x)))))",
+    "cdadar" -> "(define (cdadar x) (cdr (car (cdr (car x)))))",
+    "cdaddr" -> "(define (cdaddr x) (cdr (car (cdr (cdr x)))))",
+    "cddaar" -> "(define (cddaar x) (cdr (cdr (car (car x)))))",
+    "cddadr" -> "(define (cddadr x) (cdr (cdr (car (cdr x)))))",
+    "cdddar" -> "(define (cdddar x) (cdr (cdr (cdr (car x)))))",
+    "cddddr" -> "(define (cddddr x) (cdr (cdr (cdr (cdr x)))))",
+    "reverse" -> """(define (reverse l)
+   (if (null? l)
+       ()
+       (append (reverse (cdr l))
+               (list (car l)))))""",
+//      "map" ->  """(define (map f l)
+//  (if (null? l)
+//      l
+//      (if (pair? l)
+//          (cons (f (car l)) (map f (cdr l)))
+//          (error "Cannot map over a non-list"))))""",
+//    "for-each" -> """(define (for-each f l)
+//  (if (null? l)
+//      #t
+//      (if (pair? l)
+//          (begin (f (car l)) (for-each f (cdr l)))
+//          (error "Cannot for-each over a non-list"))))""",
+//    "foldr" -> """(define (foldr f base lst) (foldr-aux f base lst))""",
+//    "foldr-aux" -> """(define (foldr-aux f base lst)
+//        (if (null? lst)
+//            base
+//            (f (car lst) (foldr-aux f base (cdr lst)))))""",
+//    "foldl" -> """(define (foldl f base lst) (foldl-aux f base lst))""",
+//    "foldl-aux" -> """(define (foldl-aux f base lst)
+//        (if (null? lst)
+//            base
+//            (foldl-aux f (f base (car lst)) (cdr lst))))"""
+  )
+
+  val primNames: Set[String] = primDefs.keySet
+
+  def addPrelude(exp: SchemeExp): SchemeExp = {
+    var prelude: Set[SchemeExp] = Set()
+    var work: List[Expression] = List(exp)
+    var visited: List[String] = List()
+    var calls = 0
+
+    while (work.nonEmpty) {
+      val hd :: tl = work
+      work = tl
+      hd match {
+        case Identifier(name, _) if primNames.contains(name) =>
+          calls = calls+1
+          if (!visited.contains(name)) {
+            // println(s"Found primitive: $name")
+            val exp = SchemeParser.parse(primDefs(name))
+            prelude = prelude + exp
+            work = exp :: work // If a primitive depends on other primitives, make sure to also inline them.
+            visited = name :: visited
+          }
+          case e => work = e.subexpressions ::: work
+      }
+    }
+    // println(s"Distinct primitive calls: $calls")
+    SchemeBegin(prelude.toList ::: List(exp), Identity.none)
+  }
   protected def loadFile(file: String): SchemeExp =
-    SchemeParser.parse(loadFileText(file))
+    addPrelude(SchemeParser.parse(loadFileText(file)))
   // needs to be implemented to specify the testing behaviour per benchmark
   protected def onBenchmark(b: Benchmark): Unit
   // run the benchmarks
