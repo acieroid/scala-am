2486130b69f6c28e4d741c83d9effc9e578077bb
Noah Van Es <noahvanes@me.com>
2020-04-15 17:34:13 +0200

    refactored DisjointSet

--- code/shared/src/main/scala/util/DisjointSet.scala
+++ code/shared/src/main/scala/util/DisjointSet.scala
@@ -1,10 +1,11 @@
 package scalaam.util
 
-case class DisjointSet[A]
-  (var tree: Map[A,A] = Map[A,A]().withDefault(a => a),
-   val ranks: Map[A,Int] = Map[A,Int]().withDefaultValue(0)) {
+class DisjointSet[A] {
 
-  // MARK: Core methods
+  var tree: Map[A,A]    = Map[A,A]().withDefault(a => a)
+  var ranks: Map[A,Int] = Map[A,Int]().withDefaultValue(0)
+
+  // Core methods
 
   def find(a: A): A = {
     val parent = tree(a)
@@ -17,18 +18,29 @@ case class DisjointSet[A]
     }
   }
 
-  def union(r1: A, r2: A): DisjointSet[A] = if (r1 == r2) { this } else {
-    val rank1 = ranks(r1)
-    val rank2 = ranks(r2)
-    if (rank1 < rank2) {
-      this.copy(tree = tree.updated(r1,r2))
-    } else if (rank1 > rank2) {
-      this.copy(tree = tree.updated(r2,r1))
+  def union(r1: A, r2: A): A = 
+    if (r1 == r2) {
+      return r1
     } else {
-      this.copy(tree = tree.updated(r1,r2), ranks = ranks.updated(r2,rank2+1))
+      val rank1 = ranks(r1)
+      val rank2 = ranks(r2)
+      if (rank1 < rank2) {
+        tree = tree.updated(r1, r2)
+        return r2
+      } else if (rank1 > rank2) {
+        tree = tree.updated(r2, r1)
+        return r1
+      } else {
+        tree  = tree.updated(r1, r2) 
+        ranks = ranks.updated(r2, rank2 + 1)
+        return r2
+      }
     }
-  }
+
+  def merge(e1: A, e2: A) = 
+    union(find(e1),find(e2))
+  def mergeAll(elms: Iterable[A]): A = 
+    elms.tail.foldLeft(elms.head)(union)
 
   def singleton(r: A): Boolean = (ranks(r) == 0)
-
 }
