4b79d3378f52d036c6b5bc1f19e96e7c59817462
Jens Van der Plas <jevdplas@vub.be>
2020-04-16 12:31:25 +0200

    Add extra tests for R5RS spec. Comment out tests using eqv?.

--- code/shared/src/test/scala/SchemeR5RSBenchmarks.scala
+++ code/shared/src/test/scala/SchemeR5RSBenchmarks.scala
@@ -4,7 +4,7 @@ import scalaam.language.scheme.{SchemeExp, SchemeLattice}
 import scalaam.modular.ModAnalysis
 import scalaam.modular.scheme.SchemeModFSemantics
 
-object SchemePrimitiveBenchmarks {
+object SchemeR5RSBenchmarks {
 
   type Analysis = ModAnalysis[SchemeExp] with SchemeModFSemantics
   type V = (ModAnalysis[SchemeExp] with SchemeModFSemantics)#Value
@@ -14,6 +14,9 @@ object SchemePrimitiveBenchmarks {
   type L = SchemeLattice[V, A, P, Env]
 
   val bench: List[(String, L => V)] = List(
+    ("'()", _.nil),
+    ("'a", _.symbol("a")),
+    ("((if #f + *) 3 4)", _.number(12)),
     ("(* 3 4)", _.number(12)),
     ("(* 4)", _.number(4)),
     ("(*)", _.number(1)),
@@ -55,6 +58,9 @@ object SchemePrimitiveBenchmarks {
     ("(abs 0)", _.number(0)),
     ("(abs 7)", _.number(7)),
     ("(acos 1)", _.real(0.0)),
+    ("(and (= 2 2) (< 2 1))", _.bool(false)),
+    ("(and (= 2 2) (> 2 1))", _.bool(true)),
+    ("(and", _.bool(true)),
     ("(asin 0)", _.real(0.0)),
     ("(assq 'd '((a 1) (b 2) (c 3)))", _.bool(false)),
     ("(assq (list 'a) '(((a)) ((b)) ((c))))", _.bool(false)),
@@ -99,6 +105,14 @@ object SchemePrimitiveBenchmarks {
     ("(equal? 1 2)", _.bool(false)),
     ("(equal? 2 2)", _.bool(true)),
     ("(equal? \"abc\" \"abc\")", _.bool(true)),
+    //("(eqv? #f 'nil)", _.bool(false)),
+    //("(eqv? '() '())", _.bool(true)),
+    //("(eqv? 'a 'a)", _.bool(true)),
+    //("(eqv? 'a 'b)", _.bool(false)),
+    //("(eqv? (cons 1 2) (cons 1 2))", _.bool(false)),
+    //("(eqv? (lambda () 1) (lambda () 2))", _.bool(false)),
+    //("(eqv? 100000000 100000000)", _.bool(true)),
+    //("(eqv? 2 2)", _.bool(true)),
     ("(even? -1)", _.bool(false)),
     ("(even? -2)", _.bool(true)),
     ("(even? 0)", _.bool(true)),
@@ -119,10 +133,15 @@ object SchemePrimitiveBenchmarks {
     ("(length '(a (b) (c d e)))", _.number(3)),
     ("(length '(a b c))", _.number(3)),
     ("(let ((p (lambda (x) x))) (eq? p p))", _.bool(true)),
+    ("(let ((p (lambda (x) x)))\n(eq? p p))", _.bool(true)),
+    //("(let ((p (lambda (x) x)))\n(eqv? p p))", _.bool(true)),
     ("(let ((vec (vector 'a 'b 'c))) (and (equal? (vector-ref vec 0) 'a) (equal? (vector-ref vec 1) 'b) (equal? (vector-ref vec 2) 'c)))", _.bool(true)),
     ("(let ((vec (vector 0 '(2 2 2 2) \"Anna\"))) (vector-set! vec 1 '(\"Sue\" \"Sue\")) (and (equal? (vector-ref vec 0) 0) (equal? (vector-ref vec 1) '(\"Sue\" \"Sue\")) (equal? (vector-ref vec 2) \"Anna\")))", _.bool(true)),
+    ("(let ((x '#()))\n(eq? x x))", _.bool(true)),
     ("(let ((x '(a))) (eq? x x))", _.bool(true)),
     ("(let ((x '(a))) (set-cdr! x x) (list? x))", _.bool(false)),
+    ("(let ((x '(a)))\n(eq? x x))", _.bool(true)),
+    //("(let ((x '(a)))\n(eqv? x x))", _.bool(true)),
     ("(let ((x (cons 1 2))) (set-car! x 3) (and (= (car x) 3) (= (cdr x) 2)))", _.bool(true)),
     ("(let ((x (cons 1 2))) (set-cdr! x 3) (and (= (car x) 1) (= (cdr x) 3)))", _.bool(true)),
     ("(let ((x (make-vector 0 1))) (eq? x x))", _.bool(true)),
@@ -174,12 +193,16 @@ object SchemePrimitiveBenchmarks {
     ("(odd? 0)", _.bool(false)),
     ("(odd? 1)", _.bool(true)),
     ("(odd? 101)", _.bool(true)),
+    ("(or #f #f #f)", _.bool(false)),
+    ("(or (= 2 2) (< 2 1))", _.bool(true)),
+    ("(or (= 2 2) (> 2 1)", _.bool(true)),
     ("(pair? '())", _.bool(false)),
     ("(pair? '(a b c))", _.bool(true)),
     ("(pair? (cons 'a 'b))", _.bool(true)),
     ("(positive? -1)", _.bool(false)),
     ("(positive? 0)", _.bool(false)),
     ("(positive? 1)", _.bool(true)),
+    ("(quote a)", _.symbol("a")),
     ("(quotient -6 2)", _.number(-3)),
     ("(quotient 3 5)", _.number(0)),
     ("(quotient 4 2)", _.number(2)),
--- code/shared/src/test/scala/language/scheme/SchemeR5RSTests.scala
+++ code/shared/src/test/scala/language/scheme/SchemeR5RSTests.scala
@@ -8,16 +8,16 @@ import scalaam.util.Timeout
 
 import scala.concurrent.duration._
 
-trait PrimitiveTests extends AnyPropSpec {
+trait SchemeR5RSTests extends AnyPropSpec {
 
-  import SchemePrimitiveBenchmarks._
+  import SchemeR5RSBenchmarks._
 
   def analysis(text: SchemeExp): Analysis
 
   def testExpr(program: String, answer: L => V): Unit = {
     val text = SchemeParser.parse(program)
     val a = analysis(text)
-    val l = a.lattice.asInstanceOf[SchemePrimitiveBenchmarks.L]
+    val l = a.lattice.asInstanceOf[SchemeR5RSBenchmarks.L]
 
     a.analyze(Timeout.start(Duration(30 , SECONDS)))
     assume(a.finished(), s"Analysis of $program timed out.")
@@ -25,31 +25,31 @@ trait PrimitiveTests extends AnyPropSpec {
     assert(l.subsumes(result, answer(l)), s"Primitive computation test failed on program: $program with result $result.")
   }
 
-  SchemePrimitiveBenchmarks.bench.foreach { case (e, a) => property (s"Primitive in $e is correct.") { testExpr(e, a) } }
+  SchemeR5RSBenchmarks.bench.foreach { case (e, a) => property (s"Primitive in $e is correct.") { testExpr(e, a) } }
 }
 
-class ConcreteBigStepModFSoundnessTests extends PrimitiveTests {
+class ConcreteBigStepModFSoundnessTests extends SchemeR5RSTests {
   def analysis(text: SchemeExp) = new ModAnalysis(text) with BigStepSemantics
     with PowersetDomain
     with NoSensitivity
     with StandardSchemeModFSemantics
 }
 
-class ConcreteSmallStepModFSoundnessTests extends PrimitiveTests {
+class ConcreteSmallStepModFSoundnessTests extends SchemeR5RSTests {
   def analysis(text: SchemeExp) = new ModAnalysis(text) with SmallStepSemantics
     with PowersetDomain
     with NoSensitivity
     with StandardSchemeModFSemantics
 }
 
-class TypeBigStepModFSoundnessTests extends PrimitiveTests {
+class TypeBigStepModFSoundnessTests extends SchemeR5RSTests {
   def analysis(text: SchemeExp) = new ModAnalysis(text) with BigStepSemantics
     with TypeDomain
     with NoSensitivity
     with StandardSchemeModFSemantics
 }
 
-class TypeSmallStepModFSoundnessTests extends PrimitiveTests {
+class TypeSmallStepModFSoundnessTests extends SchemeR5RSTests {
   def analysis(text: SchemeExp) = new ModAnalysis(text) with SmallStepSemantics
     with TypeDomain
     with NoSensitivity
