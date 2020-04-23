1b5b509185f8fef4fd8de0278458e386aa54f83e
Jens Van der Plas <jevdplas@vub.be>
2020-04-23 11:53:07 +0200

    Update some tests.

--- test/gambit/deriv.scm
+++ test/gambit/deriv.scm
@@ -1,9 +1,3 @@
-(define (map f l)
-  (if (null? l)
-      l
-      (if (pair? l)
-          (cons (f (car l)) (map f (cdr l)))
-          (error "Cannot map over a non-list"))))
 ;;; DERIV -- Symbolic derivation.
 
 ;;; Returns the wrong answer for quotients.
