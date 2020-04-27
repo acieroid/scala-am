677998234a4d90fdc63b59a91d266b658aeae420
Jens Van der Plas <jevdplas@vub.be>
2020-04-27 09:11:11 +0200

    Fix eceval

--- test/icp/icp_7_eceval.scm
+++ test/icp/icp_7_eceval.scm
@@ -371,29 +371,6 @@
 (define (compiled-procedure-entry c-proc) (cadr c-proc))
 (define (compiled-procedure-env c-proc) (caddr c-proc))
 
-
-;;**original (unmonitored) version from section 5.2.1
-(define (make-stack)
-  (let ((s '()))
-    (define (push x)
-      (set! s (cons x s)))
-    (define (pop)
-      (if (null? s)
-        (error "Empty stack -- POP")
-        (let ((top (car s)))
-          (set! s (cdr s))
-          top)))
-    (define (initialize)
-      (set! s '())
-        'done)
-    (define (dispatch message)
-      (cond ((eq? message 'push) push)
-        ((eq? message 'pop) (pop))
-        ((eq? message 'initialize) (initialize))
-        (else (error "Unknown request -- STACK"
-                message))))
-    dispatch))
-
 (define (pop stack)
   (stack 'pop))
 
