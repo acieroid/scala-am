aebc4db28995d1d376b39458cb23f14e68e71bae
Jens Van der Plas <jevdplas@vub.be>
2020-04-29 08:57:03 +0200

    Enable default value for 'get'.

--- code/shared/src/main/scala/util/Table.scala
+++ code/shared/src/main/scala/util/Table.scala
@@ -4,34 +4,45 @@ object Table {
   /** Create a new empty table 
    * @return a new empty table
   */
-  def empty[V] = Table(Map[(String,String), V]())
+  def empty[V]: Table[V] = Table(Map[(String,String), V]())
 }
 
-case class Table[V](data: Map[(String,String), V]) {
-  /** Get an element from the table
+case class Table[V](data: Map[(String,String), V], default: Option[V] = None) {
+
+  /** Get an element from the table.
    * @param row     The name of the row
    * @param column  The name of the column
    * @return        An optional value, being either:
-   *  - None, it the table does not contain an element at the given row and column
-   *  - Some element in the table at the given row and column otherwise
+   *  - None, if the table does not contain an element at the given row and column and no default value is set.
+   *  - Some element in the table at the given row and column otherwise.
    **/
-  def get(row: String, column: String): Option[V] = data.get((row,column))
-  /** Add an element to the table 
+  def get(row: String, column: String): Option[V] = data.get((row,column)).orElse(default)
+
+  /** Add an element to the table.
    * @param row     The name of the row
    * @param column  The name of the column
    * @param elem    The element to add to the table
    * @return        A new table in which the element has been added 
    **/
-  def add(row: String, column: String, elem: V): Table[V] =
-    Table(data + ((row,column) -> elem))
+  def add(row: String, column: String, elem: V): Table[V] = Table(data + ((row,column) -> elem))
+
+  /**
+   * Add a default value that is returned by 'get' when an element is not present in the table.
+   * @param v The default value to be installed.
+   * @return  A new table with the given default value.
+   */
+  def withDefaultValue(v: V): Table[V] = Table(data, Some(v))
+
   /** Return all rows of a given table
    * @return The set of all row names in the table 
    **/
   def allRows: Set[String] = data.keySet.map(_._1)
+
   /** Return all columns of a given table
    * @return The set of all column names in the table
    **/
   def allColumns: Set[String] = data.keySet.map(_._2)
+
   /** Extract the given rows and columns from a table, converting it to a simpler representation (for further processing)
    * @param rows      A list of row names to include (in the given order) (default: all table rows in alphabetical order)
    * @param columns   A list of column names to include (in the given order) (default: all table columns in alphabetical order)
@@ -53,6 +64,7 @@ case class Table[V](data: Map[(String,String), V]) {
     }
     firstRow :: otherRows
   }
+
   /** Generate LaTeX code to produce the table
    * @param rows      A list of row names to include (in the given order) (default: all table rows in alphabetical order)
    * @param columns   A list of column names to include (in the given order) (default: all table columns in alphabetical order)
@@ -72,6 +84,7 @@ case class Table[V](data: Map[(String,String), V]) {
     val footerStr = "\\bottomrule\n\\end{tabular}\n\\end{table}"
     headerStr ++ content ++ footerStr
   }
+
   /** Generate a CSV representation of the given table
    * @param rows      A list of row names to include (in the given order) (default: all table rows in alphabetical order)
    * @param columns   A list of column names to include (in the given order) (default: all table columns in alphabetical order)
@@ -87,6 +100,7 @@ case class Table[V](data: Map[(String,String), V]) {
                   format: V => String = _.toString()): String =
     extract(rows,columns,default,rowName,format).map(_.mkString(","))
                                                 .mkString("\n")
+
   /** Generate a string formatted as a table (useable for pretty printing)
    * @param rows      A list of row names to include (in the given order) (default: all table rows in alphabetical order)
    * @param columns   A list of column names to include (in the given order) (default: all table columns in alphabetical order)
