7b90a28fcb8b9d05bada69e6df7c5983a96e0f2a
Quentin Stiévenart <quentin.stievenart@gmail.com>
2020-05-06 10:54:45 +0200

    Check precision equality for RQ2

--- code/jvm/src/main/scala/benchmarks/precision/PrimitivesPrecision.scala
+++ code/jvm/src/main/scala/benchmarks/precision/PrimitivesPrecision.scala
@@ -5,9 +5,11 @@ import scalaam.lattice._
 import scalaam.language.scheme._
 import scalaam.modular._
 import scalaam.modular.scheme._
-import scalaam.language.scheme.primitives.SchemePrelude
+import scalaam.language.scheme.primitives._
 import scala.concurrent.duration._
 import scalaam.util._
+import scalaam.core._
+
 
 object PrimitivesBenchmarks {
 
@@ -27,12 +29,12 @@ object PrimitivesBenchmarks {
   )
 
   val benchmarks = {
-    SchemeBenchmarks.gambit ++
-    SchemeBenchmarks.gabriel ++
-    SchemeBenchmarks.scp1_compressed ++
+//    SchemeBenchmarks.gambit ++
+//    SchemeBenchmarks.gabriel ++
+//    SchemeBenchmarks.scp1_compressed ++
     SchemeBenchmarks.icp ++
     // SchemeBenchmarks.ad ++
-    List("test/ad/all.scm") ++
+//    List("test/ad/all.scm") ++
     standard ++
     List()
   }
@@ -143,24 +145,9 @@ abstract class PrimitivesComparison extends AnalysisComparison[
       runBenchmark(b)
     })
     println("Results:")
-    println(results.prettyString(format = _.map(_.toString()).getOrElse("TIMEOUT")))
-    /**
-    println("Benchmark & 0 & CS & 2CS & 2ACS & 10CS & 10ACS & FA & 2FA & 10FA & CSFA & Max \\\\")
-    this.results.toSeq.sortBy(_._1).foreach { case (b, r) =>
-      val refined_0_0 = r.getOrElse("0_0", Some("T")).getOrElse("T")
-      val refined_CS_0 = r.getOrElse("CS_0", Some("T")).getOrElse("T")
-      val refined_2CS_0 = r.getOrElse("2CS_0", Some("T")).getOrElse("T")
-      val refined_2AcyclicCS_0 = r.getOrElse("2AcyclicCS_0", Some("T")).getOrElse("T")
-      val refined_10CS_0 = r.getOrElse("10CS_0", Some("T")).getOrElse("T")
-      val refined_10AcyclicCS_0 = r.getOrElse("10AcyclicCS_0", Some("T")).getOrElse("T")
-      val refined_FA_0 = r.getOrElse("FA_0", Some("T")).getOrElse("T")
-      val refined_2FA_0 = r.getOrElse("2FA_0", Some("T")).getOrElse("T")
-      val refined_10FA_0 = r.getOrElse("10FA_0", Some("T")).getOrElse("T")
-      val refined_CSFA_0 = r.getOrElse("CSFA_0", Some("T")).getOrElse("T")
-      val concrete = r.getOrElse("concrete", Some("T")).getOrElse("T")
-      println(s"$b & $refined_0_0 & $refined_CS_0 & $refined_2CS_0 & $refined_2AcyclicCS_0 & $refined_10CS_0 & $refined_10AcyclicCS_0 & $refined_FA_0 & $refined_2FA_0 & $refined_10FA_0 & $refined_CSFA_0 & $concrete\\\\")
-    }
-    */
+    val columns = List("0_0", "CS_0", "2CS_0", "2ACS_0", "10CS_0", "10ACS_0", "FA_0", "2FA_0", "10FA_0", "CSFA_0", "concrete")
+    println(results.withDefaultValue(None).prettyString(columns = columns, format = _.map(_.toString()).getOrElse("TIMEOUT")))
+    println(results.withDefaultValue(None).toLatexString(columns = columns, format = _.map(_.toString()).getOrElse("--")))
   }
 }
 
@@ -236,6 +223,211 @@ object PrimitivesComparisonRQ1 extends PrimitivesComparison {
     override def toString() = "CSFA_0"
     override val primPrecision = SchemePrelude.primNames
   }
+}
+
+
+/** Checks that results produced with manual primitives and preluded primitives
+  * exactly match, using a simple type lattice */
+object PrimitivesComparisonRQ2 {
+  type Benchmark = String
+
+  sealed trait BaseAddr extends Address { def printable = true }
+  case class VarAddr(vrb: Identifier) extends BaseAddr { override def toString = s"<variable $vrb>" }
+  case class PrmAddr(nam: String)     extends BaseAddr { override def toString = s"<primitive $nam>" }
+  case class CarAddr(idn: Identity)   extends BaseAddr { override def toString = s"<car $idn>" }
+  case class CdrAddr(idn: Identity)   extends BaseAddr { override def toString = s"<cdr $idn>" }
+  case class RetAddr(idn: Identity)   extends BaseAddr { override def toString = s"<return $idn>" }
+  case class PtrAddr(idn: Identity)   extends BaseAddr { override def toString = s"<pointer $idn>" }
+
+  private def convertAddr(analysis: Analysis)(addr: analysis.Addr): BaseAddr = addr match {
+    case analysis.ComponentAddr(_, analysis.VarAddr(v)) => VarAddr(v)
+    case analysis.ComponentAddr(_, analysis.PrmAddr(n)) => PrmAddr(n)
+    case analysis.ComponentAddr(_, analysis.PtrAddr(e)) => PtrAddr(e.idn)
+    case analysis.ComponentAddr(_, analysis.CarAddr(e)) => CarAddr(e.idn)
+    case analysis.ComponentAddr(_, analysis.CdrAddr(e)) => CdrAddr(e.idn)
+    case analysis.ReturnAddr(cmp) => RetAddr(analysis.view(cmp).body.idn)
+  }
+
+  type Analysis = ModAnalysis[SchemeExp] with SchemeModFSemantics {
+    val valueLattice: TypeSchemeLattice[Addr, Component]
+  }
+  type BaseValue = baseDomain.L
+  val baseDomain = new TypeSchemeLattice[BaseAddr, Unit]
+  val baseLattice = baseDomain.schemeLattice
+
+  case class StubPrimitive(name: String) extends SchemePrimitive[BaseValue, BaseAddr] {
+    def call(fpos: SchemeExp, args: List[(SchemeExp, BaseValue)], store: Store[BaseAddr,BaseValue], alloc: SchemeAllocator[BaseAddr]) =
+      throw new Exception("Stub primitive: call not supported")
+  }
+  case class LambdaIdnEq(lambda: SchemeLambdaExp) extends SchemeLambdaExp {
+    def idn = lambda.idn
+    def args = lambda.args
+    def body = lambda.body
+    def varArgId = lambda.varArgId
+    override def hashCode() = lambda.idn.hashCode()
+    override def equals(that: Any) = that match {
+      case LambdaIdnEq(l) => l.idn == this.lambda.idn
+      case _ => false
+    }
+  }
+  private def convertValue(analysis: Analysis)(value: analysis.Value): BaseValue = value match {
+    case analysis.valueLattice.L(str, bool, num, char, sym, nil, prims, clos, consCells) =>
+      baseDomain.L(str, bool, num, char, sym, nil,
+        prims.map(p => StubPrimitive(p.name)),
+        clos
+          .map({
+            case ((e, env), name) =>
+              /* Replace the expression, but keep its position.
+               This is to avoid situations where we have the same expression, but it's parsed differently due to preludation (i.e., primitives/global) */
+              val e2: SchemeLambdaExp = SchemeLambda(List(), List(SchemeValue(scalaam.language.sexp.ValueNil, e.idn)), e.idn)
+              ((e2, ()), name) })
+          .filter({
+            /* Drops primitives that could have been preluded in the other analysis */
+            case (_, Some(name)) => !SchemePrelude.primNames.contains(name)
+            case _ => true
+          }),
+        consCells.map({ case (car, cdr) => (convertAddr(analysis)(car), convertAddr(analysis)(cdr)) }))
+  }
+
+  type BaseStore = Map[BaseAddr, BaseValue]
+
+  /**
+    * Joins two stores
+    * @param b1 a base store
+    * @param b2 a base store
+    * @return the joined base store
+    * */
+  protected def join(b1: BaseStore, b2: BaseStore): BaseStore =
+    b2.foldLeft(b1) {
+      case (acc, (addr2,value2)) =>
+        val value1 = acc.getOrElse(addr2, baseLattice.bottom)
+        val joined = baseLattice.join(value1,value2)
+        acc + (addr2 -> joined)
+    }
+
+  /**
+    *  Compare two stores, assuming one is more precise than the other
+    *  @param b1 the less precise store
+    *  @param b2 the more precise store
+    *  @return the set of addresses that have been refined in b2 w.r.t. b1
+    */
+  protected def checkEquality(b1: BaseStore, b2: BaseStore): Unit =
+    b1.foreach { case (addr1,value1) =>
+      val value2 = b2.getOrElse(addr1,baseLattice.bottom)
+      if (value1 != value2) {
+        if (value2 == baseLattice.bottom) {
+          /* We ignore this case. That's not entirely correct though. It corresponds to
+           preluded primitive code in b1, that is not reached in b2 because
+           there is no preludation for that primitive */
+          ()
+        } else {
+          println(s"Addresses do not match: $addr1: $value1 vs $value2")
+          // println(s"Str equal? ${value1.str == value2.str}")
+          // println(s"Bool equal? ${value1.bool == value2.bool}")
+          // println(s"Num equal? ${value1.num == value2.num}")
+          // println(s"Char equal? ${value1.char == value2.char}")
+          // println(s"Sym equal? ${value1.sym == value2.sym}")
+          // println(s"Nil equal? ${value1.nil == value2.nil}")
+          // println(s"Prims equal? ${value1.prims == value2.prims}")
+          // println(s"Clos equal? ${value1.clos == value2.clos}")
+          // println(s"Cons equal? ${value1.consCells == value2.consCells}")
+          assert(false)
+        }
+      }
+    }
+
+  /**
+    *  Given an analysis (that terminated), extract its "base store": a mapping from base addresses to base values
+    *  That is, convert the resulting store into one within the (context-insensitive) base domain
+    *  @param analysis the analysis from which the results need to be extracted
+    *  @return a store in the base domain
+    */
+  protected def extract(analysis: Analysis): BaseStore =
+    analysis.store.groupBy(p => convertAddr(analysis)(p._1)).view
+      .filterKeys(!_.isInstanceOf[PrmAddr])
+      .mapValues(m => analysis.lattice.join(m.values))
+      .mapValues(convertValue(analysis))
+      .toMap
+
+  /**
+    * Run the analysis on a given program
+    *
+    * @param analysis a function that creates an analysis for a given program
+      * @param program the program to analyze
+    * @param name the name for the analysis (used for reporting to the console)
+    * @param path the name of / path to the benchmark program to run
+    * @param timeout (optional) the timeout
+    * @return an option value, being:
+    *     - the base store if the analysis terminated
+    *     - `None` otherwise
+    */
+  protected def runAnalysis(analysis: SchemeExp => Analysis, name: String, program: SchemeExp, path: Benchmark, timeout: Timeout.T = Timeout.none): Option[BaseStore] = {
+    try {
+      val anl = analysis(program)
+      println(s"... analysing $path using $name ...")
+      anl.analyze(timeout)
+      if (anl.finished()) {
+        Some(extract(anl))
+      } else {
+        println("... timed out")
+        None
+      }
+    } catch {
+      case e: Exception =>
+        println(s"Analyzer failed with exception $e")
+        None
+      case e: VirtualMachineError =>
+        System.gc()
+        println(s"Analyzer failed with error $e")
+        None
+    }
+  }
+
+  /** Run a benchmark
+    *  @param benchmark the benchmark program to run
+    */
+  def runBenchmark(benchmark: Benchmark) = {
+    val txt = Reader.loadFile(benchmark)
+    val prg = SchemeParser.parse(txt)
+    forBenchmark(benchmark, prg)
+  }
+
+
+  import scalaam.modular.scheme.CompoundSensitivities.SeparateLowHighSensitivity._
+  import scalaam.modular.scheme.CompoundSensitivities.SeparateLowHighSensitivity.Sensitivity._
+
+  def analysis1(p: SchemeExp) = new scalaam.cli.evaluation.primitives.PerformanceType.AnalysisWithPreludedPrimitives(p) with S_CSFA_0 {
+    override def toString = "P"
+  }
+  def analysis2(p: SchemeExp) = new scalaam.cli.evaluation.primitives.PerformanceType.AnalysisWithManualPrimitives(p) with S_CSFA_0 {
+    override def toString = "M"
+  }
+
+  def analysisTimeout() = Timeout.start(Duration(2, MINUTES))
+
+  def forBenchmark(path: Benchmark, program: SchemeExp) = {
+    runAnalysis(analysis1, "P", program, path, analysisTimeout) match {
+      case Some(result1) =>
+        runAnalysis(analysis2, "M", program, path, analysisTimeout) match {
+          case Some(result2) =>
+            checkEquality(result1, result2)
+            println("match!")
+          case None => ()
+        }
+      case None => ()
+    }
+  }
+
+  val benchmarks = PrimitivesBenchmarks.standard
+
+  def runBenchmarks() = {
+    benchmarks.foreach(b => {
+      System.gc()
+      runBenchmark(b)
+    })
+  }
+  def main(args: Array[String]) =
+    runBenchmarks()
 }
 
 abstract class PrimitivesComparisonRQ3 extends PrimitivesComparison {
