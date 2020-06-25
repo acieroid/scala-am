8615f7c07bd6bea091e5e08a071d36f71d22c3f9
Jens Van der Plas <jevdplas@vub.be>
2020-06-25 11:22:29 +0200

    Fix error in WL algorithm.

--- code/shared/src/main/scala/modular/SequentialWorklistAlgorithm.scala
+++ code/shared/src/main/scala/modular/SequentialWorklistAlgorithm.scala
@@ -17,8 +17,12 @@ trait SequentialWorklistAlgorithm[Expr <: Expression] extends ModAnalysis[Expr] 
     // do the intra-analysis
     val intra = intraAnalysis(current)
     intra.analyze(timeout)
-    intra.commit()
-    workList = workList.tail // By doing this here, we ensure the analysis can be correctly restarted after a timeout.
+    if (!timeout.reached) {
+      // Only do the following steps when a timeout is not reached (i.e. when there was not enough time to complete the intra-analysis).
+      // By doing this here, we ensure the analysis can be correctly restarted after a timeout. Should happen before commit, since commit will add new elements to the worklist.
+      workList = workList.tail
+      intra.commit()
+    }
   }
   // step until worklist is empty or timeout is reached
   def analyze(timeout: Timeout.T = Timeout.none): Unit =
