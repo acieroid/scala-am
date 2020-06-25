56b5eae95a3fbe85cea45ff7983cc71538bb4a7a
Noah Van Es <noahvanes@me.com>
2020-06-25 18:56:17 +0200

    remove hack in sequential worklist algorithm

--- code/shared/src/main/scala/modular/SequentialWorklistAlgorithm.scala
+++ code/shared/src/main/scala/modular/SequentialWorklistAlgorithm.scala
@@ -14,13 +14,15 @@ trait SequentialWorklistAlgorithm[Expr <: Expression] extends ModAnalysis[Expr] 
   def step(timeout: Timeout.T): Unit = {
     // take the next component
     val current = workList.head
+    workList = workList.tail 
     // do the intra-analysis
     val intra = intraAnalysis(current)
     intra.analyze(timeout)
-    if (!timeout.reached) {
-      // Only do the following steps when a timeout is not reached (i.e. when there was not enough time to complete the intra-analysis).
-      // By doing this here, we ensure the analysis can be correctly restarted after a timeout. Should happen before commit, since commit will add new elements to the worklist.
-      workList = workList.tail
+    if (timeout.reached) {
+      // analysis timed out => we need to add it to the worklist again
+      addToWorkList(current)
+    } else {
+      // analysis finished properly => commit its changes to the global analysis state
       intra.commit()
     }
   }
