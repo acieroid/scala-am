3e4b778d96be8972aae68a3b5e3f00140d16c320
Jens Van der Plas <jevdplas@vub.be>
2020-07-09 11:36:45 +0200

    Implement properties for CodeChange.

--- code/shared/src/main/scala/scalaam/language/change/ChangeExp.scala
+++ code/shared/src/main/scala/scalaam/language/change/ChangeExp.scala
@@ -9,10 +9,10 @@ case object CHA extends Label // Change
 trait ChangeExp extends CSchemeExp
 
 case class CodeChange(old: SchemeExp, nw: SchemeExp, idn: Identity) extends ChangeExp {
-  def fv: Set[String] = throw new Exception("Free variable detection not allowed for code change expressions.")
+  def fv: Set[String] = old.fv ++ nw.fv
   def label: Label = CHA
   def subexpressions: List[Expression] = List(old, nw)
-  override val height: Int = Integer.MIN_VALUE
+  override val height: Int = Math.max(old.height, nw.height) + 1
 }
 
 object CodeVersion extends Enumeration {
