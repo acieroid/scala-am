bf8641fda9b4877cdf555b516bc8579f3441b017
Jens Van der Plas <jevdplas@vub.be>
2020-07-13 14:34:23 +0200

    ... also for futures.

--- test/concurrentScheme/futures/pc.scm
+++ test/concurrentScheme/futures/pc.scm
@@ -1,31 +1,39 @@
 ;; Producer-consumer problem
-(define N (+ 10 42))
-(define NCONS 42)
-(define buffer (atom '()))
-(define done (atom #f))
+(define N (+ 10 (random 42)))
+(define NCONS (random 42))
+(define buffer (ref '())) ; Protected by lock.
+(define done (ref #f))
 (define lock (new-lock))
+(define done-lock (new-lock))
 (define (do-something element)
   (display element) (newline))
 (define (producer n)
   (if (= n 0)
-      (reset! done #t)
-      (begin
-        (acquire lock)
-        (reset! buffer (cons n (read buffer)))
-        (release lock)
-        (producer (- n 1)))))
+    (begin
+      (acquire done-lock)
+      (ref-set done #t)
+      (release done-lock))
+    (begin
+      (acquire lock)
+      (ref-set buffer (cons n (deref buffer)))
+      (release lock)
+      (producer (- n 1)))))
 (define (consumer)
-  (if (null? (read buffer))
-      (begin
-        (if (read done)
-            'done
-            (consumer)))
-      (begin
-        (acquire lock)
-        (do-something (car (read buffer)))
-        (reset! buffer (cdr (read buffer)))
-        (release lock)
-        (consumer))))
+  (acquire lock)
+  (if (null? (deref buffer))
+    (begin
+      (release lock)
+      (acquire done-lock)
+      (let ((res (if (deref done)
+                     'done
+                   (consumer))))
+        (release done-lock)
+        res))
+    (begin
+      (do-something (car (deref buffer)))
+      (ref-set buffer (cdr (deref buffer)))
+      (release lock)
+      (consumer))))
 
 (define producer-thrd (future (producer N)))
 
