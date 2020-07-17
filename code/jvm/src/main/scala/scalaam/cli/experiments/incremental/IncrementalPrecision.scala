70959331015729b854ddceb938013f30d23d4aa4
Jens Van der Plas <jevdplas@vub.be>
2020-07-17 16:16:18 +0200

    Precision benchmarks.

--- code/jvm/src/main/scala/scalaam/cli/experiments/incremental/IncrementalPrecision.scala
+++ code/jvm/src/main/scala/scalaam/cli/experiments/incremental/IncrementalPrecision.scala
@@ -1,12 +1,12 @@
 package scalaam.cli.experiments.incremental
 
 import scalaam.bench.scheme.IncrementalSchemeBenchmarkPrograms
-import scalaam.cli.experiments.precision.PrecisionBenchmarks
-import scalaam.core.Expression
+import scalaam.core._
 import scalaam.language.CScheme.CSchemeParser
 import scalaam.language.change.CodeVersion._
 import scalaam.language.scheme.SchemeExp
 import scalaam.modular.incremental.scheme.AnalysisBuilder._
+import scalaam.modular.scheme._
 import scalaam.util.Reader
 import scalaam.util.Writer._
 import scalaam.util.benchmarks.{Table, Timeout}
@@ -65,15 +65,13 @@ trait IncrementalPrecision[E <: Expression] extends IncrementalExperiment[E] {
       results = results.add(file, eq, "∞").add(file, mp, "∞").add(file, lp, "∞")
       return
     }
-    writeln("finished.")
+    write("finished.")
 
     // Both analyses normally share the same lattice, allocation schemes,... which makes it unnecessary to convert values etc.
     val iStore = a1.store.withDefaultValue(a1.lattice.bottom)
     val rStore = a2.store.withDefaultValue(a2.lattice.bottom)
-  //  println(iStore)
-   // println(rStore)
 
-    val allAddr = iStore.keySet ++ rStore.keySet
+    val allAddr = iStore.keySet.filter(interestingAddress) ++ rStore.keySet.filter(interestingAddress)
     var e = 0L
     var l = 0L
     var m = 0L
@@ -85,30 +83,36 @@ trait IncrementalPrecision[E <: Expression] extends IncrementalExperiment[E] {
       } else if (a1.lattice.subsumes(incr, rean.asInstanceOf[a1.Value])) {
         l += 1 // The incremental value subsumes the value of the full reanalysis => less precise.
       } else {
-        println(incr + " " + rean)
         m += 1 // The incremental value is subsumed by the value of the full reanalysis => more precise.
       }
     })
     results = results.add(file, eq, e.toString).add(file, mp, m.toString).add(file, lp, l.toString)
   }
 
+  def interestingAddress[A <: Address](a: A): Boolean
   def reportError(file: String): Unit = results = results.add(file, eq, "E").add(file, mp, "E").add(file, lp, "E")
   val output: String = s"benchOutput/results-incremental-precision-${System.currentTimeMillis()}.txt"
   def createOutput(): String = results.prettyString(columns = List(eq, mp, lp))
 }
 
-object IncrementalSchemeModFPrecision extends IncrementalPrecision[SchemeExp] {
-  override def benchmarks(): Set[String] = IncrementalSchemeBenchmarkPrograms.sequential
-  override def analysis(e: SchemeExp): Analysis = new IncrementalSchemeModFAnalysis(e)
+trait IncrementalSchemePrecision extends IncrementalPrecision[SchemeExp] {
+  override def interestingAddress[A <: Address](a: A): Boolean = a match {
+    case PrmAddr(_) => false
+    case _ => true
+  }
   override def parse(string: String): SchemeExp = CSchemeParser.parse(Reader.loadFile(string))
   override def timeout(): Timeout.T = Timeout.start(Duration(2, MINUTES))
 }
 
-object IncrementalSchemeModConcPrecision extends IncrementalPrecision[SchemeExp] {
+object IncrementalSchemeModFPrecision extends IncrementalSchemePrecision {
+  override def benchmarks(): Set[String] = IncrementalSchemeBenchmarkPrograms.sequential
+  override def analysis(e: SchemeExp): Analysis = new IncrementalSchemeModFAnalysis(e)
+
+}
+
+object IncrementalSchemeModConcPrecision extends IncrementalSchemePrecision {
   override def benchmarks(): Set[String] = IncrementalSchemeBenchmarkPrograms.concurrent
   override def analysis(e: SchemeExp): Analysis = new IncrementalModConcAnalysis(e)
-  override def parse(string: String): SchemeExp = CSchemeParser.parse(Reader.loadFile(string))
-  override def timeout(): Timeout.T = Timeout.start(Duration(2, MINUTES))
 }
 
 object IncrementalSchemeModXPrecision extends App {
